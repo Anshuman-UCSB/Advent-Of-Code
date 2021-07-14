ac92390eea8b3f096dcee4526ab2e9bad2b4d1d7
Anshuman Dash <adash@spacex.com>
2021-07-14 11:58:24 -0700

    Day 6 part 1

--- 2019/c++/Day 6/main.cpp
+++ 2019/c++/Day 6/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+int p1(map<string, vector<string>>& orbits,map<string, int>& depth, string node){
+	if(node == "COM"){
+		depth["COM"] = 0;
+	}else{
+		
+	}
+	int out = depth[node];
+	for(auto& c: orbits[node]){
+		depth[c] = depth[node]+1;
+		out+=p1(orbits, depth, c);
+	}
+	return out;
+	
+}
 
 int main(){
 	vector<string> inp;
@@ -12,7 +28,23 @@ int main(){
 		inp.push_back(line);
 	}
 
+	map<string, vector<string>> orbits;
+	map<string, int> depth;
 	for(auto l: inp){
-		cout<<l<<endl;
+		string a, b;
+		a = l.substr(0,l.find(")"));
+		b = l.substr(l.find(")")+1);
+		orbits[a].emplace_back(b);
 	}
+	
+	
+	// for(auto& v: orbits){
+	// 	cout<<v.first<<": [ ";
+	// 	for(auto &c: v.second){
+	// 		cout<<c<<" ";
+	// 	}cout<<"]"<<endl;
+	// }
+
+	cout<<"P1: "<<p1(orbits, depth, "COM")<<endl;
+
 }
