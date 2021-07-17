610ab510f85009df230ed94ae9bdbcf3a776062e
Anshuman Dash <adash@spacex.com>
2021-07-16 17:02:16 -0700

    Day 12 progress

--- 2019/c++/Day 12/main.cpp
+++ 2019/c++/Day 12/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include "../util.h"
+#include "../prints.h"
+#define ll long long
 
 using namespace std;
 
@@ -13,16 +15,76 @@ struct Moon{
 	Moon(string inp){
 		dx = dy = dz = 0;
 		// <x=-1, y=0, z=2>
-		// inp.pop_back();
-		// inp.push_back(',');
-		cout<<inp<<endl;
+		inp = inp.substr(3);
+		x = stoi(inp.substr(0, inp.find(",")));
+		inp = inp.substr(1+inp.find("="));
+		y = stoi(inp.substr(0, inp.find(",")));
+		inp = inp.substr(1+inp.find("="));
+		z = stoi(inp.substr(0, inp.find(">")));
+	}
+
+	ll getEnergy(){
+		return (abs(x)+abs(y)+abs(z))*(abs(dx)+abs(dy)+abs(dz));
+	}
+
+	void updateGravity(const Moon& m){
+		if(m.x>x){dx++;}
+		else if(m.x<x){dx--;}
+		if(m.y>y){dy++;}
+		else if(m.y<y){dy--;}
+		if(m.z>z){dz++;}
+		else if(m.z<z){dz--;}
+	}
+
+	void updatePos(){
+		x+=dx;
+		y+=dy;
+		z+=dz;
 	}
 };
+ostream& operator<<(ostream& os, const Moon& m)
+{
+    os<<"("<<m.x<<", "<<m.y<<", "<<m.z<<")	<"<<m.dx<<", "<<m.dy<<", "<<m.dz<<">";
+    return os;
+}
+
+string hashState(vector<Moon>& moons, int var){
+	string out = "";
+	for(auto& m: moons){
+		switch(var){
+			case 0:out+=m.x+"_"+m.dx+",";break;
+			case 1:out+=m.y+"_"+m.dy+",";break;
+			case 2:out+=m.z+"_"+m.dz+",";break;
+		}
+	}
+	return out;
+}
 
 int main(){
 	fstream file("Day 12/input");
 	string line;
-	while(getline(file, line)){
-		Moon m(line);
+	vector<Moon> moons;
+	while(getline(file, line))
+		moons.emplace_back(line);
+	
+	for(int it = 1;it<=1000;it++){
+		// cout<<BOLD<<RED<<"Step "<<it<<RESET<<endl;
+		for(auto& m: moons){
+			for(auto& n: moons){
+				m.updateGravity(n);
+			}
+		}
+		for(auto& m: moons){
+			m.updatePos();
+			// cout<<"	"<<m<<endl;
+		}
 	}
+	ll p1 = 0;
+	for(auto& m : moons){
+		p1+=m.getEnergy();
+	}
+	cout<<"[P1] "<<p1<<endl;
+
+	ll xCycle;
+	for()
 }
