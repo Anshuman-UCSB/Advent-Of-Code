81a1bc4ebc96509f7510b2cc744e123452aa7eb3
Anshuman Dash <adash@spacex.com>
2021-07-23 13:28:57 -0700

    21 ms, screw directed graph theory 8)

--- 2019/c++/Day 14/main.cpp
+++ 2019/c++/Day 14/main.cpp
@@ -3,6 +3,9 @@
 #include <map>
 #include <sstream>
 #include <fstream>
+#define ll long long
+#define ORECOUNT 1000000000000
+// #define ORECOUNT 100000000
 
 struct reagent{
 	string item;
@@ -73,7 +76,7 @@ void initRecipies(){
 	}
 }
 
-map<string, int> counts;
+map<string, ll> counts;
 void printCounts(){
 	for(auto p: counts){
 		cout<<p.first<<", "<<p.second<<endl;
@@ -90,14 +93,74 @@ void make(const reagent& r){
 		}
 	}
 }
+bool make2(const reagent& r, ll mult){
+	// cout<<"make called on "<<r<<endl;
+	
+	counts[r.item] -= r.amt*mult; //use up r.amt of r.item
+	if(r.item == "ORE" && counts[r.item] < -ORECOUNT){
+		return false;
+	}
+	if(r.item != "ORE" && counts[r.item]<0){
+		ll newMult;
+		// for(newMult=1;recipies[r.item].out.amt*newMult+counts[r.item]<0;newMult++){}
+		ll k;
+		for(k = -counts[r.item];k%recipies[r.item].out.amt!= 0;k++){}
+		k/=recipies[r.item].out.amt;
+		newMult = k;
+		if(newMult != k){
+			cout<<"ERROR: "<<newMult<<" = "<< k/recipies[r.item].out.amt<<endl;
+		}
+		// newMult = k/recipies[r.item].out.amt;		
+		
+		counts[r.item] += recipies[r.item].out.amt*newMult;
+		for(auto& reg: recipies[r.item].inp){
+			if(!make2(reg, newMult)){
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
 void p1(){
 	// counts["FUEL"] = -1; //request 1 fuel to be made
-	make(reagent("FUEL", 1));
+	make2(reagent("FUEL", 1), 1);
 	cout<<"[P1] "<<-counts["ORE"]<<endl;
+}
+
+void p2(){
+	counts.clear();
+	int i;
+	make2(reagent("FUEL", 1), 1);
+	for(i = 2;make2(reagent("FUEL", 1), i/2);i*=2){
+		// cout<<"Upper limit of "<<i<<endl;
+		// printCounts();
+		// string trash;
+		// cin>>trash;
+	}
+	// cout<<"Upper limit of "<<i<<endl;
+	counts.clear();
+	int l, h;
+	l = i/2;
+	h = i;
+	int m;
+	while(l+1<h){
+		counts.clear();
+		m = (l+h)/2;
+		if(make2(reagent("FUEL", 1), m)){
+			l = m;
+			// cout<<"Could make "<<m<<" fuel."<<endl;
+		}else{
+			// cout<<"Could not make "<<m<<" fuel."<<endl;
+			h = m-1;
+		}
+		// cout<<l<<"->"<<h<<endl;
+	}
+	cout<<"[P2] "<<h<<endl;
 }
 
 int main(){
 	initRecipies();
 	p1();
+	p2();
 }
