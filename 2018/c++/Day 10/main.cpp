6bc6e8bd6f11cf1ac54868136e9fec15ea1bf6d6
Anshuman Dash <adash@spacex.com>
2021-08-03 16:15:22 -0700

    Restart

--- 2018/c++/Day 10/main.cpp
+++ 2018/c++/Day 10/main.cpp
@@ -16,12 +16,36 @@ struct light{
 
 Point low, high;
 
-
-static inline void iter(vector<light>& lights){
-	for(auto& l: lights)
-		l.iter();
+vector<light> eval(vector<light>& lights, int time){
+	low = high = Point();
+	vector<light> out(lights.size());
+	for(int i = 0;i<lights.size();i++){
+		out[i] = lights[i];
+		out[i].pos+= time*out[i].vel;
+		
+		updateRanges(low, high, out[i].pos);
+	}
+	return out;
 }
 
+void print(vector<light>& lights){
+	set<Point> points;
+	for(auto& v: lights){
+		points.insert(v.pos);
+	}
+	cout<<" 	";
+	for(int x = low.x;x<=high.x;x++){
+		cout<<abs(x%10);
+	}cout<<endl;
+	for(int y = low.y; y<=high.y; y++){
+		cout<<" "<<y<<"	";
+		for(int x = low.x;x<=high.x;x++){
+			if(points.count(Point(x,y))){
+				cout<<"#";
+			}else cout<<" ";
+		}cout<<endl;
+	}
+}
 
 int main(){
 	fstream file("Day 10/input");
@@ -30,8 +54,21 @@ int main(){
 	vector<light> lights;
 
 	while(getline(file, line)){
-		stringstream ss(line);
-		ss>>line>>x>>line>>y>>line>>line>>dx>>line>>dy;
+		stringstream ss(line.substr(10));
+		ss>>x>>line>>y;
+		ss.seekg(12, ios_base::cur);
+		ss>>dx>>line>>dy;
 		lights.emplace_back(x, y, dx, dy);
 	}
+	int oldDist = 9999999;
+	int dist = oldDist-1;
+	int p2;
+	for(p2 = 1;dist<oldDist;p2++){
+		oldDist = dist;
+		lights = eval(lights, 1);
+		dist = low.distTo(high);
+	}
+	lights = eval(lights, -1);
+	print(lights);
+	cout<<p2<<endl;
 }
--- 2018/c++/point.h
+++ 2018/c++/point.h
@@ -36,9 +36,10 @@ struct Point{
 		return *this;
 	}
 
-	Point operator+(const Point& p){
+	Point operator+(const Point& p) const{
 		return Point(x+p.x, y+p.y);
 	}
+
 	Point operator-(const Point& p){
 		return Point(x-p.x, y-p.y);
 	}
@@ -64,6 +65,25 @@ struct Point{
 		return abs(p.y-y)+abs(p.x-x);
 	}
 };
+
+Point operator*(const Point& p, const int& sc){
+	return Point(sc*p.x, sc*p.y);
+}
+
+Point operator*(const int& sc, const Point& p){
+	return Point(sc*p.x, sc*p.y);
+}
+
+void updateRanges(Point& low, Point& high, const Point& p){
+	if((low.x || low.y) == 0){
+		low = p;
+	}else
+		low.min(p);
+	if((high.x || high.y) == 0){
+		high = p;
+	}else
+		high.max(p);
+}
 
 ostream& operator<<(ostream& os, const Point& p){
 	os<<"("<<p.x<<", "<<p.y<<")";
