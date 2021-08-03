c35df25066be048bc41c93eca2a5369da9b51a25
Anshuman Dash <adash@spacex.com>
2021-08-03 12:38:06 -0700

    2.2ms both parts

--- 2018/c++/Day 8/main.cpp
+++ 2018/c++/Day 8/main.cpp
@@ -3,13 +3,18 @@
 
 vector<int> regs;
 
+map<int, int> cache;
+
+int globalId = 1;
 struct Node{
+	int id;
 	int childN, metaN;
 	vector<Node> children;
 	vector<int> meta;
 
 	Node():childN(0), metaN(0){}
 	Node(int& i){
+		id = globalId++;
 		childN = regs[i++];
 		metaN = regs[i++];
 		for(int j = 0;j<childN;j++){
@@ -30,12 +35,33 @@ struct Node{
 		}
 		return out;
 	}
+
+	int p2(){
+		if(cache.count(id))
+			return cache[id];
+		if(childN == 0){
+			for(auto& v: meta){
+				cache[id]+=v;
+			}
+		} else{
+			for(auto& ind: meta){
+				if(0<ind && ind<=childN){
+					cache[id]+=children[ind-1].p2();
+				}
+			}
+		}
+
+		return cache[id];
+	}
 };
 
+Node head;
 void p1(){
-	int pos = 0;
-	Node a(pos);
-	cout<<"[P1] "<<a.sumMeta()<<endl;
+	cout<<"[P1] "<<head.sumMeta()<<endl;
+}
+
+void p2(){
+	cout<<"[P2] "<<head.p2()<<endl;
 }
 
 int main(){
@@ -47,6 +73,8 @@ int main(){
 	while(ss>>c){
 		regs.push_back(c);
 	}
-
+	int pos = 0;
+	head = Node(pos);
 	p1();
+	p2();
 }
