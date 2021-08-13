39df6e5e2682afe45334f8ce18d79d9f2cd3f6d2
Anshuman Dash <adash@spacex.com>
2021-08-13 15:24:45 -0700

    Setup cmake!

--- 2017/c++/build/CMakeFiles/advent2017.dir/DependInfo.cmake
+++ 2017/c++/build/CMakeFiles/advent2017.dir/DependInfo.cmake
@@ -1,40 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/adash/Advent-Of-Code/2017/c++/src/day01.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day01.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day02.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day02.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day03.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day03.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day04.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day04.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day05.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day05.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day06.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day06.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day07.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day07.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day08.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day08.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day09.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day09.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day10.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day10.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day11.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day11.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day12.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day12.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day13.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day13.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day14.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day14.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day15.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day15.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day16.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day16.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day17.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day17.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day18.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day18.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day19.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day19.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day20.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day20.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day21.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day21.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day22.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day22.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day23.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day23.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day24.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day24.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/day25.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/day25.cpp.o"
-  "/home/adash/Advent-Of-Code/2017/c++/src/main.cpp" "/home/adash/Advent-Of-Code/2017/c++/build/CMakeFiles/advent2017.dir/src/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adash/Advent-Of-Code/2017/c++/src/day01.cpp" "CMakeFiles/advent2017.dir/src/day01.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day01.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day02.cpp" "CMakeFiles/advent2017.dir/src/day02.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day02.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day03.cpp" "CMakeFiles/advent2017.dir/src/day03.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day03.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day04.cpp" "CMakeFiles/advent2017.dir/src/day04.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day04.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day05.cpp" "CMakeFiles/advent2017.dir/src/day05.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day05.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day06.cpp" "CMakeFiles/advent2017.dir/src/day06.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day06.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day07.cpp" "CMakeFiles/advent2017.dir/src/day07.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day07.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day08.cpp" "CMakeFiles/advent2017.dir/src/day08.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day08.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day09.cpp" "CMakeFiles/advent2017.dir/src/day09.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day09.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day10.cpp" "CMakeFiles/advent2017.dir/src/day10.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day10.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day11.cpp" "CMakeFiles/advent2017.dir/src/day11.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day11.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day12.cpp" "CMakeFiles/advent2017.dir/src/day12.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day12.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day13.cpp" "CMakeFiles/advent2017.dir/src/day13.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day13.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day14.cpp" "CMakeFiles/advent2017.dir/src/day14.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day14.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day15.cpp" "CMakeFiles/advent2017.dir/src/day15.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day15.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day16.cpp" "CMakeFiles/advent2017.dir/src/day16.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day16.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day17.cpp" "CMakeFiles/advent2017.dir/src/day17.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day17.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day18.cpp" "CMakeFiles/advent2017.dir/src/day18.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day18.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day19.cpp" "CMakeFiles/advent2017.dir/src/day19.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day19.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day20.cpp" "CMakeFiles/advent2017.dir/src/day20.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day20.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day21.cpp" "CMakeFiles/advent2017.dir/src/day21.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day21.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day22.cpp" "CMakeFiles/advent2017.dir/src/day22.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day22.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day23.cpp" "CMakeFiles/advent2017.dir/src/day23.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day23.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day24.cpp" "CMakeFiles/advent2017.dir/src/day24.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day24.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/day25.cpp" "CMakeFiles/advent2017.dir/src/day25.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/day25.cpp.o.d"
+  "/home/adash/Advent-Of-Code/2017/c++/src/main.cpp" "CMakeFiles/advent2017.dir/src/main.cpp.o" "gcc" "CMakeFiles/advent2017.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links.
--- 2017/c++/src/day01.cpp
+++ 2017/c++/src/day01.cpp
@@ -2,4 +2,8 @@
 
 void day01(input_t inp){
 	cout<<"test"<<", "<<"also";
+	vector<int> testThis(10);
+	for(int i = 0;i<10;i++){
+		testThis[i] = i+1;
+	}
 }
