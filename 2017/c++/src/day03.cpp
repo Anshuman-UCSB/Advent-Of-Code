186832f8c7abcca21db70db848dde9d67da7fef4
Anshuman Dash <adash@spacex.com>
2021-08-16 12:55:52 -0700

    57 us

--- 2017/c++/src/day03.cpp
+++ 2017/c++/src/day03.cpp
@@ -1,6 +1,55 @@
 #include "AOC.h"
 #include <cmath>
 
+int p2Solve(const int& target){
+	map<pii, int> m;
+	pii p;
+	m[p] = 1;
+	int len = 0;
+	for(;;){
+		len++;
+		for(int i = 0;i<len;i++){
+			p.x++;
+			for(int dy = -1;dy<=1;dy++)
+				for(int dx = -1;dx<=1;dx++)
+					if(dy || dx)
+						m[p]+=m[pii(p.x+dx, p.y+dy)];
+			if(m[p]>target)
+				return m[p];
+		}
+		for(int i = 0;i<len;i++){
+			p.y++;
+			for(int dy = -1;dy<=1;dy++)
+				for(int dx = -1;dx<=1;dx++)
+					if(dy || dx)
+						m[p]+=m[pii(p.x+dx, p.y+dy)];
+			if(m[p]>target)
+				return m[p];
+		}
+		len++;
+		for(int i = 0;i<len;i++){
+			p.x--;
+			for(int dy = -1;dy<=1;dy++)
+				for(int dx = -1;dx<=1;dx++)
+					if(dy || dx)
+						m[p]+=m[pii(p.x+dx, p.y+dy)];
+			if(m[p]>target)
+				return m[p];
+		}
+		for(int i = 0;i<len;i++){
+			p.y--;
+			for(int dy = -1;dy<=1;dy++)
+				for(int dx = -1;dx<=1;dx++)
+					if(dy || dx)
+						m[p]+=m[pii(p.x+dx, p.y+dy)];
+			if(m[p]>target)
+				return m[p];
+		}
+	}
+
+	return m[p];
+}
+
 chrono::time_point<std::chrono::steady_clock> day03(input_t inp){
 
 	int target = stoi(inp[0]);
@@ -46,37 +95,9 @@ chrono::time_point<std::chrono::steady_clock> day03(input_t inp){
 	}
 	out:;
 
-	map<pii, int> m;
-	pii p(0,0);
-	m[p] = 1;
-	int len = 1;
-	for(;;){
-		p.x++;
-		m[p] = m[pii(x,y-1)] + m[pii(x,y+1)] + m[pii(x-1,y)] + m[pii(x+1,y)];
-		cout<<m[p]<<endl;
-		if(m[p]>target)goto finish;
-		for(int i = 0;i<len;i++){
-			p.y++;
-			m[p] = m[pii(x,y-1)] + m[pii(x,y+1)] + m[pii(x-1,y)] + m[pii(x+1,y)];
-			cout<<m[p]<<endl;
-			if(m[p]>target)goto finish;
-		}
-		len++;
-		for(int i = 0;i<len;i++){
-			p.x--;
-			m[p] = m[pii(x,y-1)] + m[pii(x,y+1)] + m[pii(x-1,y)] + m[pii(x+1,y)];
-			cout<<m[p]<<endl;
-			if(m[p]>target)goto finish;
-		}
-		for(int i = 0;i<len;i++){
-			p.y--;
-			m[p] = m[pii(x,y-1)] + m[pii(x,y+1)] + m[pii(x-1,y)] + m[pii(x+1,y)];
-			cout<<m[p]<<endl;
-			if(m[p]>target)goto finish;
-		}
-	}
-	finish:;
+	int p2 = p2Solve(target);
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<endl;
+	cout<<"[P2] "<<p2<<endl;
 	return done;
 }
