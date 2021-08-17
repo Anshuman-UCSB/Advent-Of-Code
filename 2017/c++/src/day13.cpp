6fe48836b1f492c6cc0bcd7868f663b85d5c8582
Anshuman Dash <adash@spacex.com>
2021-08-17 10:08:17 -0700

    34ms lmao I don't wanna do Linear congruences

--- 2017/c++/src/day13.cpp
+++ 2017/c++/src/day13.cpp
@@ -1,7 +1,55 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day13(input_t& inp){
+map<int, int> m;
+int p1Solve(){
+	const int t = 0;
+	int out = 0;
+	for(auto& [depth, period]: m){
+		if((t+depth)%period == 0){
+			out+=depth*(period/2 + 1);
+		}
+	}
+	return out;
+}
 
+bool p2Help(int& t){
+	for(auto& [depth, period]: m){
+		if((t+depth)%period == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int p2Solve(){
+	for(int i = 0;;i++){
+		if(p2Help(i)) return i;
+	}
+}
+
+static inline int gcd(int x, int y){
+	int t; 
+	while(x){
+		t = x;
+		x=y%x; y = t;
+	}
+	return y;
+}
+
+chrono::time_point<std::chrono::steady_clock> day13(input_t& inp){
+	for(auto& l: inp){
+		stringstream ss(l);
+		string tr;
+		pii t;
+		ss>>t.first>>tr>>t.second;
+		t.second--;
+		t.second*=2;
+		m.insert(t);
+	}
+	int p1 = p1Solve();
+	int p2 = p2Solve();
 	auto done = chrono::steady_clock::now();
+	cout<<"[P1] "<<p1<<endl;
+	cout<<"[P2] "<<p2<<endl;
 	return done;
 }
