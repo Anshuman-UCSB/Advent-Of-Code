8bf8f0b159698137d2bd91dca691350f0f10e440
Anshuman Dash <adash@spacex.com>
2021-08-18 09:18:07 -0700

    1.072ms

--- 2017/c++/src/day17.cpp
+++ 2017/c++/src/day17.cpp
@@ -1,7 +1,33 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day17(input_t& inp){
+#define SIZE 2018
 
+chrono::time_point<std::chrono::steady_clock> day17(input_t& inp){
+	int step = stoi(inp[0]);
+	// int step = 3;
+	int regs[SIZE] = {};
+	int p = 0;
+	int p1;
+	int n;
+	for(n = 1;n<2018;n++){
+		for(int _ = 0;_<step;_++){
+			p = regs[p];
+		}
+		regs[n] = regs[p];
+		regs[p] = n;
+		p = n;
+	}
+	p1 = regs[p];
+	int p2 = 0;
+	for(int p = 0, i=0; i<50000000;p++){
+		if(p == 1) p2=i;
+		int s = (i-p) / step +1;
+		p += s*(step+1)-1;
+		p %= (i+=s);
+	}
+	
 	auto done = chrono::steady_clock::now();
+	cout<<"[P1] "<<p1<<endl;
+	cout<<"[P2] "<<p2<<endl;
 	return done;
 }
