3bd660954a51b782cc0a93e01024b997025aa262
Anshuman Dash <adash@spacex.com>
2021-08-19 09:19:27 -0700

    Weirdly easy af

--- 2017/c++/src/day19.cpp
+++ 2017/c++/src/day19.cpp
@@ -2,37 +2,38 @@
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {-1,0,1,0};
+int p2=0;
 
 string p1solve(const input_t& m, pii p){
 	string path;
 
 	int dir = 2;
 	for(;;){
+		p2++;
 		if(char ch = m[p.y][p.x]; ch>='A' && ch<='Z'){
 			path+=ch;
 		}
-		cout<<p<<endl;
+		// cout<<p<<endl;
 		pii next(p.x+dx[dir], p.y+dy[dir]);
-		if(m[next.y][next.x]==' '){
+		if(m[p.y][p.x]=='+'){
 			dir = (dir+1)%4;
 			next = pii(p.x+dx[dir], p.y+dy[dir]);
 			if(m[next.y][next.x]!=' '){
+				// cout<<"turning onto '"<<m[next.y][next.x]<<"'"<<endl;
 				p = next;
 			}else{
 				dir = (dir+2)%4;
 				next = pii(p.x+dx[dir], p.y+dy[dir]);
 				if(m[next.y][next.x]!=' '){
+					// cout<<"turning on2 "<<m[next.y][next.x]<<endl;
 					p = next;
-				}else{
-					break;
 				}
 			}
 		}else{
 			p = next;
 		}
+		if(m[p.y][p.x] == ' ') return path;
 	}
-
-	return path;
 }
 
 chrono::time_point<std::chrono::steady_clock> day19(input_t& inp){
@@ -40,8 +41,11 @@ chrono::time_point<std::chrono::steady_clock> day19(input_t& inp){
 	for(;p.x<inp[0].size();p.x++){
 		if(inp[0][p.x] == '|') break;
 	}
+	inp.emplace_back(inp[0].size(), ' ');
 	string p1 = p1solve(inp, p);
-	cout<<p<<endl;
+	
 	auto done = chrono::steady_clock::now();
+	cout<<"[P1] "<<p1<<endl;
+	cout<<"[P2] "<<p2<<endl;
 	return done;
 }
