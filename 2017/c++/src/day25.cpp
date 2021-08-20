3cd023316701b647b779e21ed7b3e3f9cd744813
Anshuman Dash <adash@spacex.com>
2021-08-20 12:48:48 -0700

    25 in 8.8ms

--- 2017/c++/src/day25.cpp
+++ 2017/c++/src/day25.cpp
@@ -1,74 +1,75 @@
 #include "AOC.h"
 
-struct instr{
-	bool write;
-	bool dir;
-	int nextState;
-	instr() = default;
-	instr(bool write1, bool goRight, int next):write(write1), dir(goRight), nextState(next){}
-
-	int exec(int& cur, unordered_set<int>& regs){
-		if(write)
-			regs.insert(cur);
-		else
-			regs.erase(cur);
-
-		if(dir) cur++;
-		else	cur--;
-		
-		return nextState;
-	}
-};
-
-struct State{
-	instr on, off;
-	State() = default;
-};
-
-struct Turing{
-	unordered_set<int> tape;
-	int state;
-	int cur;
-	vector<State> states;
-	Turing(){
-		state = 0;
-		cur = 0;
-	}
-
-	void exec(){
-		if(tape.count(cur)){
-			state = states[state].on.exec(cur, tape);
-		}else{
-			state = states[state].off.exec(cur, tape);
-		}
-	}	
-};
+enum class State{A,B,C,D,E,F};
 
 chrono::time_point<std::chrono::steady_clock> day25(input_t& inp){
-	Turing cpu;
-	cpu.state = inp[0][inp[0].size()-2]-'A';
-	stringstream ss(inp[1]);
-	int steps;
-	string trsh;
-	ss>>trsh>>trsh>>trsh>>trsh>>trsh>>steps;
 
-	for(int i = 5;i<inp.size();i+=10){
-		cpu.states.emplace_back();
-		auto& s = cpu.states.back();
-		s.off.write = inp[i][22]=='1';
-		s.off.dir = (inp[i+1].size() == 33);
-		s.off.nextState = inp[i+2][26]-'A';
-		s.on.write = inp[i+4][22]=='1';
-		s.on.dir = (inp[i+5].size() == 33);
-		s.on.nextState = inp[i+6][26]-'A';
+	bool regs[100000] = {};
+	int cur = 20;
+	State s = State::A;
+	for(int i = 0;i<12459852;i++){
+		switch(s){
+			case State::A:
+				if(regs[cur]){
+					cur--;
+					s = State::E;
+				}else{
+					regs[cur++]=true;
+					s = State::B;
+				}
+				break;
+			case State::B:
+				if(regs[cur]){
+					cur++;
+					s = State::F;
+				}else{
+					regs[cur++]=true;
+					s = State::C;
+				}
+				break;
+			case State::C:
+				if(regs[cur]){
+					regs[cur++] = false;
+					s = State::B;
+				}else{
+					regs[cur--]=true;
+					s = State::D;
+				}
+				break;
+			case State::D:
+				if(regs[cur]){
+					regs[cur--] = false;
+					s = State::C;
+				}else{
+					regs[cur++] = true;
+					s = State::E;
+				}
+				break;
+			case State::E:
+				if(regs[cur]){
+					regs[cur++] = false;
+					s = State::D;
+				}else{
+					regs[cur--] = true;
+					s = State::A;
+				}
+				break;
+			case State::F:
+				if(regs[cur]){
+					cur++;
+					s = State::C;
+				}else{
+					regs[cur++] = true;
+					s = State::A;
+				}
+				break;
+		}
 	}
-
-	for(int i = 0;i<steps;i++){
-		cpu.exec();
-		// cout<<"("<<i<<","<<cpu.tape.size()<<")"<<endl;
-		// cout<<*cpu.tape.begin()<<" -> "<<*cpu.tape.rbegin()<<endl;
+	int p1 = 0;
+	for(int i = 0;i<100000;i++){
+		if(regs[i])p1++;
 	}
-	
 	auto done = chrono::steady_clock::now();
+	cout<<"[P1] "<<p1<<endl;
 	return done;
 }
