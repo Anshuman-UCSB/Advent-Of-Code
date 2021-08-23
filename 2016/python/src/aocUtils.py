f58a52f85680ba66726254f3c95b68ec213d2619
Anshuman Dash <adash@spacex.com>
2021-08-23 09:42:50 -0700

    Day 10

--- 2016/python/src/aocUtils.py
+++ 2016/python/src/aocUtils.py
@@ -1,7 +1,10 @@
-
+import re
 def printAnswer(result : tuple):
 	print("[P1]",result[0])
 	print("[P2]",result[1])
 
 def imaginaryCoordToDist(n: complex):
 	return int(abs(n.real)) + int(abs(n.imag))
+
+def readNums(inp):
+	return list(map(int, re.findall(r"(-?\d+)", inp)))
