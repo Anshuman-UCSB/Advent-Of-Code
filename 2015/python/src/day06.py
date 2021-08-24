63208d9aa4696cfad26740bf9cab21fc6f6e3a43
Anshuman Dash <adash@spacex.com>
2021-08-24 15:20:44 -0700

    np is beautiful

--- 2015/python/src/day06.py
+++ 2015/python/src/day06.py
@@ -1,3 +1,20 @@
+import numpy as np
+from src.aocUtils import readNums
 def main(input:str) -> tuple:
-	
-	return (0, 0)
+	grid = np.zeros((1000,1000))
+	grid2 = np.zeros((1000,1000))
+	for l in input.splitlines():
+		nums = readNums(l)
+		if "on" in l:
+			grid[nums[1]:1+nums[3], nums[0]:1+nums[2]]=1
+			grid2[nums[1]:1+nums[3], nums[0]:1+nums[2]]+=1
+		elif "off" in l:
+			grid[nums[1]:1+nums[3], nums[0]:1+nums[2]]=0
+			grid2[nums[1]:1+nums[3], nums[0]:1+nums[2]]-=1
+			grid2[grid2<0]+=1
+		elif "toggle" in l:
+			grid[nums[1]:1+nums[3], nums[0]:1+nums[2]]=1-grid[nums[1]:1+nums[3], nums[0]:1+nums[2]]
+			grid2[nums[1]:1+nums[3], nums[0]:1+nums[2]]+=2
+	# print(grid)
+		
+	return (int(np.sum(grid)), int(np.sum(grid2)))
