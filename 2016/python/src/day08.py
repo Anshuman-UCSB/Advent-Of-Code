dbc495515f908d2f774b782abac54c3fa2d9add9
Anshuman Dash <anshuman@ucsb.edu>
2021-08-26 02:16:30 +0000

    Couple days idk

--- 2016/python/src/day08.py
+++ 2016/python/src/day08.py
@@ -1,3 +1,17 @@
+import numpy as np
+import re
+
 def main(input:str) -> tuple:
-	
-	return (0, 0)
+	A = np.zeros([6,50], dtype = bool)
+	for l in input.splitlines():
+		a, b = map(int, re.findall(r'\d+', l))
+		id = l.split(' ')[1]
+		if id == "row": A[a] = np.roll(A[a], b)
+		elif id == "column": A.T[a] = np.roll(A.T[a], b)
+		else: A[:b,:a] = True
+	build = ""
+	for r in A:
+		build+="\n"
+		for v in r:
+			build+="#" if v else " "
+	return (np.sum(A), build)
--- 2016/python/src/day09.py
+++ 2016/python/src/day09.py
@@ -1,3 +1,37 @@
+
+def p1(string):
+	weights = [1] * len(string)
+	length = 0
+	waitUntil = 0
+	startCount = -1
+	for i in range(len(string)):
+		if(string[i] == "(" and i>=waitUntil):
+			xLoc = string.find("x", i)
+			endLoc = string.find(")", xLoc)
+			a = int(string[i+1:xLoc])
+			b = int(string[xLoc+1: endLoc])
+			for k in range(endLoc+1, endLoc+1+a):
+				weights[k]*=b
+			waitUntil = endLoc+a+1
+			startCount = endLoc
+		elif i > startCount:
+			length+=weights[i]
+	return length
+
+def p2(string):
+	weights = [1] * len(string)
+	length = 0
+	for i in range(len(string)):
+		if(string[i] == "("):
+			xLoc = string.find("x", i)
+			endLoc = string.find(")", xLoc)
+			a = int(string[i+1:xLoc])
+			b = int(string[xLoc+1: endLoc])
+			for k in range(endLoc+1, endLoc+1+a):
+				weights[k]*=b
+		elif 'A'<=string[i] and string[i]<='Z':
+			length+=weights[i]
+	return length
+
 def main(input:str) -> tuple:
-	
-	return (0, 0)
+	return (p1(input), p2(input))
