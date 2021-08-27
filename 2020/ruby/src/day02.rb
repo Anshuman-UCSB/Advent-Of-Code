823fe01c75e3e4206299d518f17d04d2f85c715f
Anshuman Dash <adash@spacex.com>
2021-08-27 14:56:35 -0700

    Kinda ugly back to python lmao

--- 2020/ruby/src/day02.rb
+++ 2020/ruby/src/day02.rb
@@ -1,11 +1,26 @@
 module Day02
 	class Day
 		def part1(input)
-			nil
+			out = 0
+			input.each {|line|
+				spl = line.split(/[- :]+/)
+				v = spl[-1].count(spl[-2])
+				if spl[0].to_i<=v && v<=spl[1].to_i
+					out+=1
+				end
+			}
+			return out
 		end
 
 		def part2(input)
-			nil
+			out = 0
+			input.each {|line|
+				spl = line.split(/[- :]+/)
+				if (spl[-1][spl[0].to_i-1]==spl[-2]) ^ (spl[-1][spl[1].to_i-1]==spl[-2])
+					out+=1
+				end
+			}
+			return out
 		end
 
 		def run(input)
