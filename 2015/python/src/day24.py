8e5b964d30a8009d13ad7e9c3ae11fb49aa581eb
Anshuman Dash <adash@spacex.com>
2021-08-27 10:05:14 -0700

    Cleaned code

--- 2015/python/src/day24.py
+++ 2015/python/src/day24.py
@@ -1,3 +1,15 @@
-def main(input:str) -> tuple:
-	
-	return (0, 0)
+from utils.aocUtils import readNums
+from itertools import combinations
+from functools import reduce
+from operator import mul
+
+def solve(nums, parts):
+	targetWeight = sum(nums) // parts
+	for n in range(1, len(nums)):
+		good = [reduce(mul, x) for x in combinations(nums, n) if sum(x) == targetWeight]
+		if good != []:
+			return min(good)
+
+def main(input:str):
+	nums = readNums(input)
+	return (solve(nums, 3),solve(nums, 4))
