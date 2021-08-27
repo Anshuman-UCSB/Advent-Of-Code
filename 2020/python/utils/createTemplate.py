abd0e217b336ad60c044208604dad3c332c5f227
Anshuman Dash <adash@spacex.com>
2021-08-27 15:04:25 -0700

    Fixed printing issue

---
+++ 2020/python/utils/createTemplate.py
@@ -0,0 +1,11 @@
+import os
+# os.mkdir("src")
+for i in range(1,26):
+	with open("src/day%02d.py" % (i), 'w') as f:
+		f.write(
+"""from utils.aocUtils import *
+def main(input:str) -> tuple:
+	p1 = 0
+	p2 = 0
+	return (p1, p2)"""
+		)
