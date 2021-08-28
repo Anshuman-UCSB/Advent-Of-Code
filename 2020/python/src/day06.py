a61ac6ff0582c7abeae31da6508e54cd020d3d0c
Anshuman Dash <anshuman@ucsb.edu>
2021-08-28 03:59:34 +0000

    xd golf

--- 2020/python/src/day06.py
+++ 2020/python/src/day06.py
@@ -1,5 +1,6 @@
 from utils.aocUtils import *
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	lines = list(map(lambda x: x.splitlines(), input.split("\n\n")))
+	p1 = sum([len(reduce(lambda a, b: a.union(b), map(set, x))) for x in lines])
+	p2 = sum([len(reduce(lambda a, b: a.intersection(b), map(set, x))) for x in lines])
 	return (p1, p2)
