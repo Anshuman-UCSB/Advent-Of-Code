f8c07b9ff58995e88fe65e031b4aacb859330d03
Anshuman Dash <anshuman@ucsb.edu>
2021-08-28 03:38:15 +0000

    regex <3

--- 2020/python/src/day03.py
+++ 2020/python/src/day03.py
@@ -1,5 +1,20 @@
 from utils.aocUtils import *
+def eval(inp, dx):
+	out = 0
+	# print()
+	for i, v in enumerate(inp.splitlines()):
+		# print((i*dx)%len(v), i)
+		if v[(i*dx)%len(v)] == '#':
+			out+=1
+	return out
+def slope2(inp):
+	out = 0
+	for i, v in enumerate(inp.splitlines()):
+		if i%2==0 and v[int(i/2)%len(v)] == '#':
+			out+=1
+	return out
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	print([eval(input, x) for x in [1,3,5,7]])
+	print(slope2(input))
+	return (eval(input, 3), prod([eval(input, x) for x in [1,3,5,7]])*slope2(input))
--- 2020/python/src/day04.py
+++ 2020/python/src/day04.py
@@ -1,5 +1,65 @@
 from utils.aocUtils import *
+import re
+def p1f(n):
+	for req in ["byr","iyr","eyr","hgt","hcl","ecl","pid"]:
+		if req not in n:
+			return False
+	return True
+
+def byr(s):
+	try:
+		return 1920<=int(re.findall(r".*byr:(\d{4})\s", s)[0])<=2002
+	except IndexError:
+		return False
+def iyr(s):
+	try:
+		return 2010<=int(re.findall(r".*iyr:(\d{4})\s", s)[0])<=2020
+	except IndexError:
+		return False
+def eyr(s):
+	try:
+		return 2020<=int(re.findall(r".*eyr:(\d{4})\s", s)[0])<=2030
+	except IndexError:
+		return False
+
+def hgt(s):
+	try:
+		m = re.findall(r"hgt:(\d+)(cm|in)\s",s)[0]
+		if m[1] == 'cm':
+			return 150<=int(m[0])<=193
+		else:
+			return 59<=int(m[0])<=76		
+	except IndexError:
+		return False
+
+def hcl(s):
+	try:
+		m = re.findall(r"hcl:#[0-9a-f]{6}\s",s)[0]
+		return True
+	except IndexError:
+		return False
+def ecl(s):
+	try:
+		m = re.findall(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\s",s)[0]
+		return True
+	except IndexError:
+		return False
+def pid(s):
+	try:
+		m = re.findall(r"pid:\d{9}\s",s)[0]
+		return True
+	except IndexError:
+		return False
+
+def p2f(s):
+	for f in [byr, iyr, eyr, hgt, hcl, ecl, pid]:
+		if not f(s+" "):
+			return False
+	return True
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	lines = input.split("\n\n")
+	p1 = len(list(filter(p1f, lines)))
+	p2 = len(list(filter(p2f, lines)))
+	
 	return (p1, p2)
