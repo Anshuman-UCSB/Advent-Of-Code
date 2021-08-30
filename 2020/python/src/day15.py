5884cd0fd1b48e91e564358d92f46b978ad6b82a
Anshuman Dash <adash@spacex.com>
2021-08-30 12:51:05 -0700

    off by 1 ew

--- 2020/python/src/day15.py
+++ 2020/python/src/day15.py
@@ -1,5 +1,26 @@
 from utils.aocUtils import *
+from time import sleep
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	nums = readNums(input)
+	cache = {}
+	last = -1
+	for i in range(30000000):
+		if i < len(nums):
+			last = nums[i]
+			cache[last] = i
+		else:
+			if last in cache:
+				# print(cache)
+				t = i-cache[last]-1
+				cache[last] = i-1
+				last = t
+			else:
+				cache[last] = i-1
+				last = 0
+		if i == 2020-1:
+			p1 = last
+	return (p1, last)
+
+#0, 3, 6
+#0, 3, 6, 0, 3, 3, 1, 0, 4, 0
