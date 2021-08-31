3fe9cd295e1cf21ffdd1f477952acda485ec21b0
Anshuman Dash <adash@spacex.com>
2021-08-31 10:45:28 -0700

    Recursive descent parser put on hold lol

--- 2020/python/src/day18.py
+++ 2020/python/src/day18.py
@@ -1,5 +1,62 @@
 from utils.aocUtils import *
+
+def eval1(eq):
+	# print("EVAL:",eq)
+	if '(' not in eq:
+		spl = eq.split(" ")
+		stack = []
+		ops = []
+		for s in spl:
+			# print(s, stack, ops)
+			if s not in ['+', '*']:
+				if ops == []:
+					stack.append(int(s))
+				else:
+					op = ops.pop()
+					if op == '*':
+						stack[-1]*=int(s)
+					else:
+						stack[-1]+=int(s)
+			else:
+				ops.append(s)
+		return stack[0]
+	else:
+		levels = [""]
+		for c in eq:
+			if c == '(':
+				levels.append('')
+			elif c == ')':
+				t = str(eval1(levels.pop()))
+				levels[-1]+=t
+			else:
+				levels[-1]+=c
+		return eval1(levels[0])
+
+def eval2(eq):
+	# print("EVAL:",eq)
+	if '(' not in eq:
+		spl = eq.split(" ")
+		stack = []
+		ops = []
+		for c in stack:
+			if c == '*':
+				ops.append('*')
+			else:
+				if ops == []:
+					stack.append(c)
+	else:
+		levels = [""]
+		for c in eq:
+			if c == '(':
+				levels.append('')
+			elif c == ')':
+				t = str(eval2(levels.pop()))
+				levels[-1]+=t
+			else:
+				levels[-1]+=c
+		return eval2(levels[0])
+
 def main(input:str):
-	p1 = 0
+	p1 = sum([eval1(l) for l in input.splitlines()])
 	p2 = 0
 	return (p1, p2)
