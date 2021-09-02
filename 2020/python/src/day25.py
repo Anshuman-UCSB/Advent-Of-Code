969d8f434f00af213ffc528251a6bceea7ce1387
Anshuman Dash <adash@spacex.com>
2021-09-02 14:53:07 -0700

    ALL YEARS DONE IN THIS REPO.

--- 2020/python/src/day25.py
+++ 2020/python/src/day25.py
@@ -1,5 +1,19 @@
 from utils.aocUtils import *
+
+def transform(subject, loopSize):
+	return pow(subject, loopSize+1, 20201227)
+
+def crack1(target):
+	i = 0
+	t = 7
+	while True:
+		if t == target:
+			return i
+		i+=1
+		t = modMult(t, 7, 20201227)
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	target = int(input.splitlines()[0])
+	door = int(input.splitlines()[1])
+	p1 = transform(door, crack1(target))
+	return (p1, None)
