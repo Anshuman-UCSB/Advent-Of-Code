1650ef421a15c6adb83ff09aced6dea0f4bfec80
Anshuman Dash <adash@spacex.com>
2021-09-02 11:43:26 -0700

    .

--- 2020/python/src/day21.py
+++ 2020/python/src/day21.py
@@ -1,5 +1,35 @@
 from utils.aocUtils import *
+from collections import defaultdict
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	counter = defaultdict(int)
+	foods = {}
+	for l in input.splitlines():
+		lhs, rhs = l.split(" (")
+		for l in lhs.split():
+			counter[l]+=1
+		ingredients = set(lhs.split())
+		allergens = set()
+		for w in rhs.split()[1:]:
+			allergens.add(w[:-1])
+		# print(ingredients, allergens)
+		for a in allergens:
+			if a not in foods:
+				foods[a] = ingredients
+			else:
+				foods[a] = foods[a].intersection(ingredients)
+	done = False
+	solved = set()
+	m = {}
+	while not done:
+		done = True
+		for a, i in foods.items():
+			i = i.difference(solved)
+			# print(a, i)
+			if len(i) == 1:
+				done = False
+				solved.add(list(i)[0])
+				m[list(i)[0]] = a
+	p1 = (sum([v for x, v in counter.items() if x not in solved]))
+	p2 = ",".join([x[0] for x in sorted(list(m.items()), key = lambda x: x[1])])
 	return (p1, p2)
