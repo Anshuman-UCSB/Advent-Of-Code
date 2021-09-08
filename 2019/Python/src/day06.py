4be39ec65a8da9898d0fbea700a8492f0241286b
Anshuman Dash <adash@spacex.com>
2021-09-08 10:43:11 -0700

    sets <3

--- 2019/Python/src/day06.py
+++ 2019/Python/src/day06.py
@@ -1,5 +1,23 @@
 from utils.aocUtils import *
+from collections import defaultdict
+
+def p1s(children, node, depth = 0):
+	return depth + sum(map(lambda x: p1s(children, x, depth+1), children[node]))
+
+def getPath(parent, node):
+	if node == 'COM':
+		return []
+	else:
+		return getPath(parent, parent[node])+[parent[node]]
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	children = defaultdict(list)
+	parent = {}
+	for l in input.splitlines():
+		b, o = l.split(")")
+		children[b].append(o)
+		parent[o] = b
+	# print(children)
+	p1 = p1s(children, "COM")
+	p2 = len(set(getPath(parent, "YOU")).symmetric_difference(set(getPath(parent, "SAN"))))
 	return (p1, p2)
