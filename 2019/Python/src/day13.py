503ec5e4784f6440239317809ba42b5d4e006fbe
Anshuman Dash <adash@spacex.com>
2021-09-08 14:52:58 -0700

    growing as a coder

--- 2019/Python/src/day13.py
+++ 2019/Python/src/day13.py
@@ -1,5 +1,18 @@
 from utils.aocUtils import *
+from src.intcode import *
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	c = CPU(input)
+	c.run()
+	p1 = c.outputs[2::3].count(2)
+	c = CPU(input)
+	c.regs[0] = 2
+	for i in range(1564, 1604):
+		c.regs[i] = 3 #make big paddle
+	while not c.done:
+		try:
+			c.run()
+		except EmptyInput:
+			c.push(0)
+	p2 = c.outputs[-1]
 	return (p1, p2)
