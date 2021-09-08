55b469fdbecc1c1d1c83e7a0843f6011af6e89e7
Anshuman Dash <adash@spacex.com>
2021-09-08 09:51:13 -0700

    grown

--- 2019/Python/src/day03.py
+++ 2019/Python/src/day03.py
@@ -1,5 +1,31 @@
 from utils.aocUtils import *
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	dx = {
+		"R":1,
+		"U":1j,
+		"L":-1,
+		"D":-1j
+	}
+	pathA,pathB = {}, {}
+	p = 0
+	step = 1
+	for c in input.splitlines()[0].split(","):
+		for _ in range(int(c[1:])):
+			p+=dx[c[0]]
+			pathA[p]=step
+			step+=1
+	
+	p = 0
+	step = 1
+	for c in input.splitlines()[1].split(","):
+		for _ in range(int(c[1:])):
+			p+=dx[c[0]]
+			pathB[p]=step
+			step+=1
+	
+	intersections = set(pathA.keys()).intersection(set(pathB.keys()))
+	print(intersections)
+
+	p1 = min(map(imaginaryCoordToDist,intersections))
+	p2 = min(map(lambda x: pathA[x]+pathB[x],intersections))
 	return (p1, p2)
