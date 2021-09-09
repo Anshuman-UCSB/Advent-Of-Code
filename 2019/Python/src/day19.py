8c17e6c3a0af6e748135e92407ae7d9204d6079b
Anshuman Dash <adash@spacex.com>
2021-09-09 14:50:19 -0700

    Don't care efficiency

--- 2019/Python/src/day19.py
+++ 2019/Python/src/day19.py
@@ -1,5 +1,34 @@
 from utils.aocUtils import *
+from src.intcode import *
+
+def testSquare(input, x, y):
+	c = CPU(input)
+	c.push(x)	
+	c.push(y)
+	c.run(True)
+	return c.pop()
+
+def canFitAtY(input, y, get_x = False):
+	x = 0
+	while testSquare(input, x,y+99) == 0:
+		x+=1
+	return not not testSquare(input, x+99, y) if get_x == False else x
+
 def main(input:str):
 	p1 = 0
-	p2 = 0
+	for y in range(50):
+		for x in range(50):
+			p1+=testSquare(input, x, y)
+	h = 100
+	while not canFitAtY(input, h):
+		h<<=1
+	l=h>>1
+	while l<h:
+		m = (l+h)>>1
+		if canFitAtY(input, m):
+			h = m
+		else:
+			l = m+1
+	
+	p2 = l+10000*canFitAtY(input, l,True)
 	return (p1, p2)
