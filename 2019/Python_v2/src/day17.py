05c8ee0b7face39a186fc61f2578af2d74aa27b1
Anshuman Dash <anshuman@ucsb.edu>
2021-12-01 04:22:54 +0000

    waiting for 2021

--- 2019/Python_v2/src/day17.py
+++ 2019/Python_v2/src/day17.py
@@ -1,5 +1,23 @@
 from utils.aocUtils import *
+from src.intcode import *
 def main(input:str):
+	c = CPU(input)
+	c.run()
+	inp = ""
+	for c in c.outputQ:
+		inp += chr(c)
+	grid = inp.splitlines()
 	p1 = 0
+	grid.pop(-1)
+	for y, row in enumerate(grid):
+		for x, val in enumerate(row):
+			if(val == '#'):
+				if (x>0 and x<len(grid[0])-1):
+					if (y>0 and y<len(grid)-1):
+						if(grid[y+1][x] == '#' and
+						   grid[y-1][x] == '#' and
+						   grid[y][x+1] == '#' and
+						   grid[y][x-1] == '#'):
+						   p1+=y*x
 	p2 = 0
 	return (p1, p2)
