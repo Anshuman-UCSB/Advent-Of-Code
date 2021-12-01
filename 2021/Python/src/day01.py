448abe8b533a9c0f5aa196e02edc7eb7f7359979
Anshuman Dash <anshuman@ucsb.edu>
2021-12-01 05:06:59 +0000

    147 for p2

--- 2021/Python/src/day01.py
+++ 2021/Python/src/day01.py
@@ -1,5 +1,13 @@
 from utils.aocUtils import *
 def main(input:str):
+	nums = readNums(input)
 	p1 = 0
+	for i,n in enumerate(nums):
+		if n>nums[i-1]:
+			p1+=1
+	sliding = [nums[n]+nums[n+1]+nums[n+2] for n in range(len(nums)-2)]
 	p2 = 0
+	for i,n in enumerate(sliding):
+		if n>sliding[i-1]:
+			p2+=1
 	return (p1, p2)
