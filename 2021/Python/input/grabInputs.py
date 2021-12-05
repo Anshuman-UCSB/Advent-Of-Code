06f0ccd9822f1259a734ed9e652a941fd54d170f
Anshuman Dash <anshuman@ucsb.edu>
2021-12-05 07:35:09 +0000

    Cleaned template

--- 2021/Python/input/grabInputs.py
+++
@@ -1,9 +0,0 @@
-import requests as re
-
-year = 2019
-
-cookie = {"session":"***REMOVED***"}
-for i in range(1,26):
-	with open("day{:0>2}.txt".format(i), 'w') as f:
-		r = re.get(f"https://adventofcode.com/{year}/day/{i}/input", cookies=cookie)
-		f.write(r.text)
