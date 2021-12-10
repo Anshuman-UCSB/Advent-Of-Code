2d2e41b05b3a4c2717924b24da529484ea72840c
Anshuman Dash <anshuman@ucsb.edu>
2021-12-10 22:52:10 +0000

    unchained

--- 2021/Python/src/day10.py
+++ 2021/Python/src/day10.py
@@ -1,5 +1,28 @@
 from utils.aocUtils import *
+from statistics import median
+def p2s(l):
+	scores = {')':1, ']':2, '}':3, '>':4}
+	s = 0
+	for c in l:
+		s = s*5+scores[c]
+	return s
+
 def main(input:str):
 	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	incompletes = []
+	for l in input.splitlines():
+		corrupted = False
+		needs = {"(":")","{":"}","<":">","[":"]"}
+		scores = {')':3, ']':57,'}':1197,'>':25137}
+		s = []
+		for c in l:
+			if c in needs:
+				s.append(needs[c])
+			else:
+				if s.pop(-1) != c:
+					p1+=scores[c]
+					corrupted = True
+					break
+		if not corrupted:
+			incompletes.append(s[::-1])
+	return (p1, median([p2s(c) for c in incompletes]))
--- 2021/c++/src/day10.cpp
+++ 2021/c++/src/day10.cpp
@@ -1,10 +1,47 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day10(input_t& inp){
-	int p1(0), p2(0);
+ull p2s(stack<char>& s){
+	ull out = 0;
+	while(!s.empty()){
+		out *= 5;
+		char t = s.top();
+		s.pop();
+		switch(t){
+			case '>':out++;
+			case '}':out++;
+			case ']':out++;
+			case ')':out++;
+		}
+	}
+	return out;
+}
 
-	
-	
+chrono::time_point<std::chrono::steady_clock> day10(input_t& inp){
+	ull p1(0);
+
+	vector<ull> incomplete;
+	map<char, char> closers = {{'(',')'}, {'[',']'}, {'{','}'}, {'<','>'}};
+	map<char, int> illegal = {{')',3}, {']',57}, {'}',1197}, {'>',25137}};
+	for(auto& l: inp){
+		bool invalid = false;
+		stack<char> s;
+		for(auto c: l){
+			if(c == '(' || c == '[' || c == '{' || c == '<')
+				s.push(closers[c]);
+			else{
+				if(c != s.top()){
+					invalid = true;
+					p1+=illegal[c];
+					break;
+				}
+				s.pop();
+			}
+		}
+		if(!invalid)
+			incomplete.push_back(p2s(s));
+	}
+	sort(begin(incomplete), end(incomplete));
+	ull p2 = incomplete[incomplete.size()/2];
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
