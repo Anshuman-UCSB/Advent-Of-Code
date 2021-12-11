ff2d280a07ba69fd8eb047b190989840cd57cded
Anshuman Dash <anshuman@ucsb.edu>
2021-12-11 07:30:03 +0000

    Done.

--- 2021/Python/src/day11.py
+++ 2021/Python/src/day11.py
@@ -1,5 +1,32 @@
 from utils.aocUtils import *
+
+def getNeighbors(p):
+	neighbors = [1,-1, 1+1j, 1j, -1+1j, 1-1j, -1j, -1-1j]
+	return [p+n for n in neighbors]
+
+def update(g):
+	for p in g:
+		g[p]+=1
+	loop = True
+	while loop:
+		loop = False
+		for c in [p for p in g if g[p] > 9]:
+			loop = True
+			g[c] = 0
+			for n in getNeighbors(c):
+				if n in g and g[n] != 0:
+					g[n]+=1
+	for p in g:
+		if g[p]>9:
+			g[p] = 0
+	return sum(1 for p in g if g[p] == 0)
+
 def main(input:str):
 	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	g = gridify(input, int)
+	for t in range(100):
+		p1+=update(g)
+	t = 101
+	while update(g) != 100:
+		t+=1
+	return (p1, t)
--- 2021/c++/src/day11.cpp
+++ 2021/c++/src/day11.cpp
@@ -1,10 +1,39 @@
 #include "AOC.h"
 
+int iter(vector<vector<int>>& m){
+	queue<pii> q;
+	int out = 0;
+	for(int y = 0;y<10;y++)
+		for(int x = 0;x<10;x++)
+			if(++m[y][x] > 9)
+				q.push(pii(x, y));
+
+	while(!q.empty()){
+		pii p = q.front();
+		q.pop();
+		if(m[p.y][p.x] > 9){
+			m[p.y][p.x] = 0;
+			out++;
+			for(int dy = -1;dy<=1;dy++)
+				for(int dx = -1;dx<=1;dx++)
+					if((dx || dy) && (p.x+dx>=0 && p.x+dx<10 && p.y+dy>=0 && p.y+dy<10) && m[p.y+dy][p.x+dx] && ++m[p.y+dy][p.x+dx]>9)
+						q.push(pii(p.x+dx, p.y+dy));
+		}
+	}
+	return out;
+}
+
 chrono::time_point<std::chrono::steady_clock> day11(input_t& inp){
 	int p1(0), p2(0);
-
-	
-	
+	vector<vector<int>> m;
+	for(auto& l: inp){
+		m.emplace_back();
+		for(auto c: l)
+			m.back().push_back(c-'0');			
+	}
+	for(int i = 0;i<100;i++)
+		p1+=iter(m);
+	for(p2=101;iter(m)!=100;p2++);	
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
