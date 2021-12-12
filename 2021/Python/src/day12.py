7299e5ab10779198d847fd1f81ad3ab2431ee96e
Anshuman Dash <anshuman@ucsb.edu>
2021-12-12 06:31:10 +0000

    :( 1 second dreams

--- 2021/Python/src/day12.py
+++ 2021/Python/src/day12.py
@@ -1,5 +1,41 @@
 from utils.aocUtils import *
+
+p1 = 0
+p2 = 0
+
+def BFS(paths, path):
+	global p1
+	pos = path[-1]
+	if(pos == 'end'):
+		p1+=1
+		return
+	for n in paths[pos]:
+		if n.islower() and n in path:
+			continue
+		BFS(paths, path+[n])
+
+def abletorevisit(path):
+	c = Counter([p for p in path if p.islower()])
+	return all(v<=1 for v in c.values())
+
+def BFS2(paths, path):
+	global p2
+	pos = path[-1]
+	if(pos == 'end'):
+		p2+=1
+		return
+	for n in paths[pos]:
+		if n == "start":
+			continue
+		if not n.islower() or n not in path or abletorevisit(path):
+			BFS2(paths, path+[n])
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
+	paths = defaultdict(list)
+	for l in input.splitlines():
+		start, end = l.split('-')
+		paths[start].append(end)
+		paths[end].append(start)
+	BFS(paths, ["start"])
+	BFS2(paths, ["start"])
 	return (p1, p2)
--- 2021/c++/src/day12.cpp
+++ 2021/c++/src/day12.cpp
@@ -1,10 +1,48 @@
 #include "AOC.h"
+#include <tuple>
+#define state tuple<string, unordered_set<string>, bool>
 
 chrono::time_point<std::chrono::steady_clock> day12(input_t& inp){
 	int p1(0), p2(0);
-
-	
-	
+	unordered_map<string, vector<string>> adj;
+	string a, b, pos;
+	size_t p;
+	char c;
+	for(auto& l: inp){
+		p = l.find('-');
+		a = l.substr(0,p);
+		b = l.substr(p+1);
+		if(b != "start")
+			adj[a].push_back(b);
+		if(a != "start")
+			adj[b].push_back(a);
+	}
+	queue<state> q({make_tuple("start", unordered_set<string>{"start"}, false)});
+	unordered_set<string> smalls;
+	bool doublevisit;
+	while(!q.empty()){
+		state front = q.front();
+		tie(pos, smalls, doublevisit) = front;
+		q.pop();
+		if(pos == "end"){
+			if(!doublevisit)
+				p1++;
+			p2++;
+		}else
+			for(auto& n: adj[pos]){
+				if(islower(n[0])){
+					if(smalls.find(n) != smalls.end()){
+						if(!doublevisit)
+							q.push(make_tuple(n, smalls, true));
+					}else{
+						unordered_set<string> t = smalls;
+						t.insert(n);
+						q.push(make_tuple(n, t, doublevisit));
+					}
+				}else
+					q.push(make_tuple(n, smalls, doublevisit));
+			}
+	}
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
