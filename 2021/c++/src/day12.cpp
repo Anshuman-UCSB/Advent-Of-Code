34aadbc99c4b020d117040a86b977957e171e4f1
Anshuman Dash <anshuman@ucsb.edu>
2021-12-12 23:14:34 +0000

    270 ms

--- 2021/c++/src/day12.cpp
+++ 2021/c++/src/day12.cpp
@@ -2,6 +2,29 @@
 #include <tuple>
 #define state tuple<string, unordered_set<string>, bool>
 
+void dfs(unordered_map<string, vector<string>> &adj, const string& pos, const unordered_set<string> smalls, bool doublevisit, int& p1, int& p2){
+	if(pos == "end"){
+		if(!doublevisit)
+			p1++;
+		p2++;
+		return;
+	}
+
+	for(auto& n: adj[pos]){
+		if(islower(n[0])){
+			if(smalls.find(n) != smalls.end()){
+				if(!doublevisit)
+					dfs(adj, n, smalls, true, p1, p2);
+			}else{
+				unordered_set<string> t = smalls;
+				t.insert(n);
+				dfs(adj, n, t, doublevisit, p1, p2);
+			}
+		}else
+			dfs(adj, n, smalls, doublevisit, p1, p2);
+	}
+}
+
 chrono::time_point<std::chrono::steady_clock> day12(input_t& inp){
 	int p1(0), p2(0);
 	unordered_map<string, vector<string>> adj;
@@ -17,32 +40,11 @@ chrono::time_point<std::chrono::steady_clock> day12(input_t& inp){
 		if(a != "start")
 			adj[b].push_back(a);
 	}
-	queue<state> q({make_tuple("start", unordered_set<string>{"start"}, false)});
-	unordered_set<string> smalls;
-	bool doublevisit;
-	while(!q.empty()){
-		state front = q.front();
-		tie(pos, smalls, doublevisit) = front;
-		q.pop();
-		if(pos == "end"){
-			if(!doublevisit)
-				p1++;
-			p2++;
-		}else
-			for(auto& n: adj[pos]){
-				if(islower(n[0])){
-					if(smalls.find(n) != smalls.end()){
-						if(!doublevisit)
-							q.push(make_tuple(n, smalls, true));
-					}else{
-						unordered_set<string> t = smalls;
-						t.insert(n);
-						q.push(make_tuple(n, t, doublevisit));
-					}
-				}else
-					q.push(make_tuple(n, smalls, doublevisit));
-			}
-	}
+	stack<state> q({make_tuple("start", unordered_set<string>{"start"}, false)});
+	string start = "start";
+	unordered_set<string> small = {"start"};
+	bool doublevisit = false;
+	dfs(adj, start, small, doublevisit, p1, p2);
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
