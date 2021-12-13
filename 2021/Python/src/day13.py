5738a66f813f2d5d97acc88a22cf0e5618421891
Anshuman Dash <anshuman@ucsb.edu>
2021-12-13 05:44:14 +0000

    Day 13 done!

--- 2021/Python/src/day13.py
+++ 2021/Python/src/day13.py
@@ -1,5 +1,41 @@
 from utils.aocUtils import *
+
+def foldx(points, x):
+	out = set()
+	for p in points:
+		if p.real > x:
+			out.add(int(x-(p.real-x))+int(p.imag)*1j)
+		else:
+			out.add(p)
+	return out
+def foldy(points, y):
+	out = set()
+	for p in points:
+		if p.imag > y:
+			out.add(int(p.real)+int(y-(p.imag-y))*1j)
+		else:
+			out.add(p)
+	return out
+
 def main(input:str):
 	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	points = set()
+	pts = True
+	for l in input.splitlines():
+		if l == "":
+			pts = False
+			continue
+		if pts:
+			nums = readNums(l)
+			points.add(nums[0]+nums[1]*1j)
+		else:
+			if l.split()[2][0] == 'y':
+				points = foldy(points, readNums(l)[0])
+				if p1 == 0:
+					p1 = len(points)
+			else:
+				points = foldx(points, readNums(l)[0])
+				if p1 == 0:
+					p1 = len(points)
+	
+	return (p1, printSet(points))
--- 2021/c++/src/day13.cpp
+++ 2021/c++/src/day13.cpp
@@ -1,10 +1,63 @@
 #include "AOC.h"
+#define grid unordered_set<pii, pair_hash>
 
+struct pair_hash{
+	template <class T1, class T2>
+	size_t operator()(pair<T1, T2> const &pair) const{
+		return hash<T1>()(pair.first) ^ hash<T2>()(pair.second);
+	}
+};
+
+void foldX(grid& g, int x){
+	grid n;
+	for(auto c: g)
+		if(c.x>x)
+			n.insert(pii(x*2-c.x, c.y));
+		else
+			n.insert(c);
+	g = n;
+}
+void foldY(grid& g, int y){
+	grid n;
+	for(auto c: g)
+		if(c.y>y)
+			n.insert(pii(c.x, 2*y-c.y));
+		else
+			n.insert(c);
+	g = n;
+}
 chrono::time_point<std::chrono::steady_clock> day13(input_t& inp){
-	int p1(0), p2(0);
-
+	int p1(0);
+	grid g;
+	bool coords = true;
+	char tr;
+	for(auto& l: inp){
+		if(l.empty()){
+			coords = false;
+			continue;
+		}
+		if(coords){
+			pii c;
+			stringstream ss(l);
+			ss>>c.x>>tr>>c.y;
+			g.insert(c);
+		}else{
+			if(l[11] == 'x')
+				foldX(g, stoi(l.substr(13)));
+			else
+				foldY(g, stoi(l.substr(13)));
+			if(!p1) p1 = g.size();
+		}
+	}
+	vector<string> s(6, "                                       ");
+	for(auto& c: g){
+		s[c.y][c.x] = '#';
+	}
 	
-	
+	string p2;
+	for(auto& l: s){
+		p2+="\n"+l;
+	}
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
