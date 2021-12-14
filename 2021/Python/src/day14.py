a76b8821b6cfc38077ac628358bf8ce3fdd8cdeb
Anshuman Dash <anshuman@ucsb.edu>
2021-12-14 07:38:54 +0000

    Day 14

--- 2021/Python/src/day14.py
+++ 2021/Python/src/day14.py
@@ -1,5 +1,36 @@
 from utils.aocUtils import *
+
+def iter(doubles, pairs):
+	n = Counter()
+	for k, v in doubles.items():
+		if k in pairs:
+			n[k[0]+pairs[k]]+=v
+			n[pairs[k]+k[1]]+=v
+		else:
+			print(n, k)
+			print(k)
+			n[k]+=v
+	return n
+
+def score(doubles, base):
+	c = Counter([base[0], base[-1]])
+	for k, v in doubles.items():
+		for l in k:
+			c[l]+=v
+	cmn = c.most_common()
+	return (cmn[0][1]-cmn[-1][1])//2
+	
+
 def main(input:str):
 	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	pairs = {}
+	base = input.splitlines()[0]
+	doubles = Counter([base[i:i+2] for i in range(len(base)-1)])
+	for l in input.splitlines()[2:]:
+		spl = l.split()
+		pairs[spl[0]] = spl[2]
+	for r in range(40):
+		if r == 10:
+			p1 = score(doubles, base)
+		doubles = iter(doubles, pairs)
+	return (p1, score(doubles, base))
--- 2021/c++/src/day14.cpp
+++ 2021/c++/src/day14.cpp
@@ -1,10 +1,53 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day14(input_t& inp){
-	int p1(0), p2(0);
+map<string, string> transforms;
 
-	
-	
+void iter(map<string, ull> &counts){
+	map<string, ull> t;
+	for(auto& [k, v]: counts){
+		auto it = transforms.find(k);
+		if(it != transforms.end()){
+			t[k[0]+it->second]+=v;
+			t[it->second+k[1]]+=v;
+		}else
+			t[k]+=v;
+		
+	}
+	counts = t;
+}
+
+ull score(map<string, ull>& c, string& base){
+	vector<ull> chr(26);
+	for(auto& [k, v]: c){
+		chr[k[0]-'A']+=v;
+		chr[k[1]-'A']+=v;
+	}
+	chr[base[0]-'A']+=1;
+	chr[base.back()-'A']+=1;
+	ull mx(0), mn(-1);
+	for(auto& v: chr){
+		mx = max(mx, v);
+		if(v)
+			mn = min(mn, v);
+	}
+	return (mx-mn)>>1;
+}
+
+chrono::time_point<std::chrono::steady_clock> day14(input_t& inp){
+	ull p1, p2;
+	map<string, ull> counts;
+	string base = inp[0];
+	for(int i = 0;i<base.size()-1;i++)
+		counts[base.substr(i,2)]++;
+	for(int i = 2;i<inp.size();i++)
+		transforms[inp[i].substr(0,2)] = inp[i].substr(6);
+
+	for(int i = 0;i<40;i++){
+		if(i == 10)
+			p1 = score(counts, base);
+		iter(counts);
+	}
+	p2 = score(counts, base);
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
