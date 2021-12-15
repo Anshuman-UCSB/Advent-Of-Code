fbdd7bc37c61c3f67fc9437c84185758bf8383b5
Anshuman Dash <anshuman@ucsb.edu>
2021-12-15 07:04:33 +0000

    LETS GO 80 MS

--- 2021/Python/src/day15.py
+++ 2021/Python/src/day15.py
@@ -1,5 +1,20 @@
 from utils.aocUtils import *
+from random import random
+from utils.heap import Heap
 def main(input:str):
-	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	g = gridify(input, int)
+	g2 = {}
+	side = len(input.splitlines())
+	for y in range(5):
+		for x in range(5):
+			for p, v in g.items():
+				i, j = im2tup(p)
+				n = crd2im(i+x*side, j+y*side)
+				nv = (v+x+y)
+				while nv>=10:
+					nv-=9
+				g2[n] = nv
+	end = crd2im(side-1, side-1)
+	end2 = crd2im(side*5-1, side*5-1)
+
+	return (dijkstras(g,0, end, lambda x: g[x])[1], dijkstras(g2,0, end2, lambda x: g2[x])[1])
--- 2021/c++/src/day15.cpp
+++ 2021/c++/src/day15.cpp
@@ -1,9 +1,60 @@
 #include "AOC.h"
+#define state pair<int, pii>
 
 chrono::time_point<std::chrono::steady_clock> day15(input_t& inp){
-	int p1(0), p2(0);
-
+	int p1(0), p2(0), side(inp.size());
+	vector<vector<int>> g(side*5, vector<int>(side*5, 0));
+	vector<vector<bool>> seen(side*5, vector<bool>(side*5, false));
+	for(int y = 0;y<side;y++)
+		for(int x = 0;x<side;x++)
+			g[y][x] = inp[y][x]-'0';
+	for(int y = side;y<side*5;y++)
+		for(int x = 0;x<side;x++)
+			g[y][x] = (g[y-side][x] == 9)?1:g[y-side][x]+1;
+	for(int y = 0;y<side*5;y++)
+		for(int x = side;x<side*5;x++)
+			g[y][x] = (g[y][x-side] == 9)?1:g[y][x-side]+1;
 	
+	priority_queue<state, vector<state>, greater<state>> q;
+	q.push(make_pair(0, pii(0,0)));
+	int risk, x, y;
+	pii pos, p1pos(side-1, side-1), p2pos(side*5-1, side*5-1);
+	while(!q.empty()){
+		state s = q.top();
+		q.pop();
+		tie(risk, pos) = s;
+		tie(x, y) = pos;
+		if(pos == p1pos)
+			p1 = risk;
+		if(pos == p2pos){
+			p2 = risk;
+			break;
+		}
+		if(seen[y][x])
+			continue;
+		seen[y][x] = true;
+		
+		if(x>0){
+			pii t(x-1, y);
+			if(seen[y][x-1] == false)
+				q.push(make_pair(risk+g[y][x-1], t));
+		}
+		if(x<g.size()-1){
+			pii t(x+1, y);
+			if(seen[y][x+1] == false)
+				q.push(make_pair(risk+g[y][x+1], t));
+		}
+		if(y>0){
+			pii t(x, y-1);
+			if(seen[y-1][x] == false)
+				q.push(make_pair(risk+g[y-1][x], t));
+		}
+		if(y<g.size()-1){
+			pii t(x, y+1);
+			if(seen[y+1][x] == false)
+				q.push(make_pair(risk+g[y+1][x], t));
+		}
+	}
 	
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
