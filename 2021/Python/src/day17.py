fab75ab43f9c7b1d34a39c9c6d393bc55984fec5
Anshuman Dash <anshuman@ucsb.edu>
2021-12-17 06:11:08 +0000

    1ms day 17

--- 2021/Python/src/day17.py
+++ 2021/Python/src/day17.py
@@ -1,5 +1,24 @@
 from utils.aocUtils import *
+
+def testVel(vel, nums):
+	stVel = int(vel.imag)
+	lx, hx, ly, hy = nums
+	pos = 0
+	while pos.real <= hx and pos.imag >= ly:
+		pos+=vel
+		vel-= 1j if vel.real<=0 else 1+1j
+		if lx<=pos.real<=hx and ly<=pos.imag<=hy:
+			return True, sum(range(1, stVel+1))
+	return False, None
+
 def main(input:str):
 	p1 = 0
 	p2 = 0
+	nums = readNums(input)
+	for x in range(nums[1]+1):
+		for y in range(nums[2], -nums[2]):
+			bl, ans = testVel(crd2im(x, y), nums)
+			if bl:
+				p1 = max(p1, ans)
+				p2+=1
 	return (p1, p2)
--- 2021/c++/src/day17.cpp
+++ 2021/c++/src/day17.cpp
@@ -1,9 +1,36 @@
 #include "AOC.h"
 
+
 chrono::time_point<std::chrono::steady_clock> day17(input_t& inp){
 	int p1(0), p2(0);
-
-	
+	stringstream ss(inp[0].substr(15));
+	int lx, ly, hx, hy;
+	char tr;
+	ss>>lx>>tr>>tr>>hx;
+	ss.ignore(256, '=');
+	ss>>ly>>tr>>tr>>hy;
+	auto simulate = [&](int dx, int dy){
+		int x(0), y(0), ans(0);
+		while(x<=hx && y>=ly){
+			x+=dx;
+			y+=dy--;
+			ans = max(ans, y);
+			if(dx)
+				dx--;
+			if(lx<=x && x<=hx && ly<=y && y<=hy)
+				return ans;
+		}
+		return -1;
+	};
+	for(int y = ly;y<=-ly;y++){
+		for(int x = 0; x<=hx;x++){
+			int res = simulate(x, y);
+			if(res != -1){
+				p2++;
+				p1=max(p1, res);
+			}
+		}
+	}
 	
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
