111ca0f56be229d8aa393c5e3f4e5cff4a546e73
Anshuman Dash <anshuman@ucsb.edu>
2021-12-18 22:11:10 +0000

    8ms with math for p1

--- 2021/c++/src/day17.cpp
+++ 2021/c++/src/day17.cpp
@@ -2,33 +2,28 @@
 
 
 chrono::time_point<std::chrono::steady_clock> day17(input_t& inp){
-	int p1(0), p2(0);
 	stringstream ss(inp[0].substr(15));
 	int lx, ly, hx, hy;
 	char tr;
 	ss>>lx>>tr>>tr>>hx;
 	ss.ignore(256, '=');
 	ss>>ly>>tr>>tr>>hy;
+	int p1((ly*(ly+1))>>1), p2(0);
 	auto simulate = [&](int dx, int dy){
-		int x(0), y(0), ans(0);
+		int x(0), y(0);
 		while(x<=hx && y>=ly){
 			x+=dx;
 			y+=dy--;
-			ans = max(ans, y);
 			if(dx)
 				dx--;
 			if(lx<=x && x<=hx && ly<=y && y<=hy)
-				return ans;
+				return true;
 		}
-		return -1;
+		return false;
 	};
 	for(int y = ly;y<=-ly;y++){
 		for(int x = 0; x<=hx;x++){
-			int res = simulate(x, y);
-			if(res != -1){
-				p2++;
-				p1=max(p1, res);
-			}
+			p2+=simulate(x, y);
 		}
 	}
 	
