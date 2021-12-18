b25c8e51f1fbc46fed5d633f1b29c1536988085b
Anshuman Dash <anshuman@ucsb.edu>
2021-12-18 20:56:59 +0000

    Triangle number math for p1

--- 2021/Python/src/day17.py
+++ 2021/Python/src/day17.py
@@ -1,24 +1,21 @@
 from utils.aocUtils import *
 
 def testVel(vel, nums):
-	stVel = int(vel.imag)
 	lx, hx, ly, hy = nums
 	pos = 0
 	while pos.real <= hx and pos.imag >= ly:
 		pos+=vel
 		vel-= 1j if vel.real<=0 else 1+1j
 		if lx<=pos.real<=hx and ly<=pos.imag<=hy:
-			return True, sum(range(1, stVel+1))
-	return False, None
+			return True
+	return False
 
 def main(input:str):
-	p1 = 0
 	p2 = 0
 	nums = readNums(input)
+	p1 = ((nums[2])*(nums[2]+1))//2
 	for x in range(nums[1]+1):
 		for y in range(nums[2], -nums[2]):
-			bl, ans = testVel(crd2im(x, y), nums)
-			if bl:
-				p1 = max(p1, ans)
+			if testVel(crd2im(x, y), nums):
 				p2+=1
 	return (p1, p2)
