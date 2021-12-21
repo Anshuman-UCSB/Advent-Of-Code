61cf5ba3cbb3504f12e76dae1259f6220f51b325
Anshuman Dash <anshuman@ucsb.edu>
2021-12-21 09:26:03 +0000

    Python done

--- 2021/Python/src/day19.py
+++ 2021/Python/src/day19.py
@@ -1,15 +1,59 @@
 from utils.aocUtils import *
-from itertools import permutations, combinations
+from itertools import permutations, combinations, product
 
-# def hashPoints()
+def rot(coords):
+	a, b, c = coords
+
+	yield (+a, +b, +c);yield (+b, +c, +a);yield (+c, +a, +b)
+	yield (+c, +b, -a);yield (+b, +a, -c);yield (+a, +c, -b)
+	yield (+a, -b, -c);yield (+b, -c, -a);yield (+c, -a, -b)
+	yield (+c, -b, +a);yield (+b, -a, +c);yield (+a, -c, +b)
+	yield (-a, +b, -c);yield (-b, +c, -a);yield (-c, +a, -b)
+	yield (-c, +b, +a);yield (-b, +a, +c);yield (-a, +c, +b)
+	yield (-a, -b, +c);yield (-b, -c, +a);yield (-c, -a, +b)
+	yield (-c, -b, -a);yield (-b, -a, -c);yield (-a, -c, -b)
+
+def hash(a, b):
+	out=str(dist(a,b))
+	out+=","
+	out+=str(min(abs(x-y) for x,y in zip(a,b)))
+	out+=","
+	out+=str(max(abs(x-y) for x,y in zip(a,b)))
+	return out
+
+class Scanner:
+	def __init__(self, inp):
+		self.id = readNums(inp.splitlines()[0])[0]
+		self.beacons = [tuple(readNums(l)) for l in inp.splitlines()[1:]]
+		self.hashes = [hash(a,b) for a,b in permutations(self.beacons, 2)]
 
 def main(input:str):
-	p1 = 0
 	p2 = 0
-	scanners = []
-	for sn in [x.splitlines()[1:] for x in input.split("\n\n")]:
-		scanners.append([])
-		for x,y,z in [map(int,l.split(',')) for l in sn]:
-			scanners[-1].append((x,y,z))
-	print(rotate((1,2,3)))
-	return (p1, p2)
+	knownPos = {0:(0,0,0)} #scanner 0 known
+	scanners = [Scanner(x) for x in input.split("\n\n")]
+	while len(knownPos) < len(scanners):
+		for a,b in combinations(scanners,2):
+			if (len(set(a.hashes)&set(b.hashes)) == 66): #12 choose 2
+				if((a.id in knownPos) != (b.id in knownPos)):
+					if b.id in knownPos:
+						a,b = b,a
+					t = list(zip(*[list(rot(p)) for p in b.beacons]))
+					for rotset in t:
+						c = Counter()
+						for p in rotset:
+							c+=Counter([sub(n,p) for n in a.beacons])
+							ans = [k for k,v in c.items() if v>=12]
+							if len(ans):
+								knownPos[b.id]=add([k for k,v in c.items() if v>=12][0], knownPos[a.id])
+								b.beacons = rotset
+								break
+						if b.id in knownPos:
+							break
+	p1 = set()
+	for sn in scanners:
+		for p in sn.beacons:
+			p1.add(add(p, knownPos[sn.id]))
+	for a in knownPos.values():
+		for b in knownPos.values():
+			p2 = max(p2, manhattan(a,b))
+	return (len(p1), p2)
--- 2021/Python/src/day21.py
+++ 2021/Python/src/day21.py
@@ -1,5 +1,61 @@
 from utils.aocUtils import *
+from itertools import product
+
+class Dice:
+	def __init__(self):
+		self.n = 0
+		self.rolls =0
+
+	def roll(self):
+		out = mod(self.n+1,100)+mod(self.n+2,100)+mod(self.n+3,100)
+		self.n = (self.n+3)%100
+		self.rolls+=3
+		return out
+
+@cache
+def mod(n, base=10):
+	return (n-1)%base+1
+
+class player:
+	def __init__(self, pos):
+		self.pos = pos
+		self.score = 0
+	def turn(self, dice):
+		self.pos = mod(self.pos+dice.roll())
+		self.score+=self.pos
+
+possibles = [sum(c) for c in product(range(1,4), repeat=3)]
+@cache
+def play(p1pos, p1score, p2pos, p2score, turn):
+	if p1score>=21:
+		return 1
+	elif p2score>=21:
+		return 1j
+	else:
+		s = 0
+		if turn == 0:
+			for roll in possibles:
+				s+=play(mod(p1pos+roll), p1score+mod(p1pos+roll), p2pos, p2score, 1)
+		elif turn == 1:
+			for roll in possibles:
+				s+=play(p1pos, p1score,mod(p2pos+roll), p2score+mod(p2pos+roll), 0)
+		return s
+			
+
 def main(input:str):
-	p1 = 0
-	p2 = 0
-	return (p1, p2)
+	nums = readNums(input)
+	player1 = player(nums[1])
+	player2 = player(nums[3])
+	d = Dice()
+	p1t = True
+	while player1.score<1000 and player2.score<1000:
+		if p1t:
+			player1.turn(d)
+		else:
+			player2.turn(d)
+		p1t = not p1t
+
+	p1 = min(player1.score, player2.score)*d.rolls
+	p2=play(nums[1], 0, nums[3], 0, 0)
+	
+	return (p1, max(im2tup(p2)))
