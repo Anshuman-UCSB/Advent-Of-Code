1d04721a4de691ce35e57de35a075e6df27fae8b
Anshuman Dash <anshuman@ucsb.edu>
2021-12-21 11:11:50 +0000

    Psychosis kicking in

--- 2021/c++/src/day19.cpp
+++ 2021/c++/src/day19.cpp
@@ -1,186 +1,206 @@
 #include "AOC.h"
-#include <cmath>
 #include <cassert>
-#define coord tuple<int, int, int>
-#define fp tuple<int, int, int>
+#include <cmath>
+
+struct point{
+	int x, y, z;
+	point():x(0),y(0),z(0){}
+	point(int x, int y, int z):x(x), y(y),z(z){}
+};
+point abs(const point& a){
+	return point(abs(a.x), abs(a.y), abs(a.z));
+}
+int sum(const point& a){
+	return a.x+a.y+a.z;
+}
+point operator+(const point& a, const point& b){
+	return point(a.x+b.x, a.y+b.y, a.z+b.z);
+}
+point operator-(const point& a, const point& b){
+	return point(a.x-b.x, a.y-b.y, a.z-b.z);
+}
+bool operator<(const point& a, const point& b){
+	return (a.x<b.x || 
+			(a.x==b.x && a.y<b.y) ||
+			(a.x==b.x && a.y==b.y && a.z<b.z));
+}
+bool operator==(const point& a, const point& b){
+	return (a.x==b.x && a.y==b.y && a.z==b.z);
+}
+int manhattan(const point& a, const point& b){
+	point t = a-b;
+	return abs(t.x)+abs(t.y)+abs(t.z);
+}
+int dist(const point& a, const point& b){
+	point t = a-b;
+	return pow(t.x,2)+pow(t.y,2)+pow(t.z,2);
+}
+
+ostream& operator<<(ostream& os, const point& p){
+	os<<"("<<p.x<<", "<<p.y<<", "<<p.z<<")";
+	return os;
+}
+
+void valsort(int& a, int& b){
+	if(abs(a)>abs(b)){
+		int oa(a), ob(b);
+		if(b<=0){
+			b =  oa;
+			a = -ob;
+		}else{
+			b = -oa;
+			a =  ob;
+		}
+	}
+}
+void normalizeVec(point& vec){
+	valsort(vec.x, vec.z);
+	valsort(vec.x, vec.y);
+	valsort(vec.y, vec.z);
+	if(vec.x<0){
+		vec.x*=-1;
+		vec.y*=-1;
+	}
+	if(vec.y<0){
+		vec.y*=-1;
+		vec.z*=-1;
+	}
+}
+
 struct Scanner{
+	point pos;
 	int id;
-	bool solved;
-	coord pos;
-	vector<coord> beacons;
-	vector<fp> fingerprints;
-	map<fp, pair<coord, coord>> fpmap;
-	Scanner():solved(false){}
-	Scanner(string& inp):Scanner(){
+	bool solved, done;
+	vector<point> points;
+	map<int, vector<point>> fpmap;
+	set<int> fps;
+	Scanner():solved(false), done(false){}
+	Scanner(string inp):Scanner(){
 		stringstream ss(inp);
-		string t;
-		ss>>t>>t>>id;
+		ss>>inp>>inp>>id;
+		if(id == 0)
+			solved = true;
 	}
 };
 
-coord operator+(const coord& a, const coord& b){
-	auto [x1,y1,z1] = a;
-	auto [x2,y2,z2] = b;
-	return coord(x2+x1, y2+y1, z2+z1);
-}
-
-
-coord operator-(const coord& a, const coord& b){
-	auto [x1,y1,z1] = a;
-	auto [x2,y2,z2] = b;
-	return coord(x2-x1, y2-y1, z2-z1);
-}
-
-int dist(const coord& a, const coord& b){
-	auto [x1,y1,z1] = a;
-	auto [x2,y2,z2] = b;
-	return (pow(x2-x1, 2)+pow(y2-y1, 2)+pow(z2-z1, 2));
-}
-
-int manhattan(const coord& a, const coord& b){
-	auto [x1,y1,z1] = a;
-	auto [x2,y2,z2] = b;
-	return (abs(x2-x1)+abs(y2-y1)+abs(z2-z1));
-}
-int offmin(const coord& a, const coord& b){
-	int ans = 1000;
-	auto [x1, y1, z1] = a;
-	auto [x2, y2, z2] = b;
-	ans = min(ans, abs(x2-x1));
-	ans = min(ans, abs(y2-y1));
-	ans = min(ans, abs(z2-z1));
-	return ans;
-}
-int offmax(const coord& a, const coord& b){
-	int ans = 0;
-	auto [x1, y1, z1] = a;
-	auto [x2, y2, z2] = b;
-	ans = max(ans, abs(x2-x1));
-	ans = max(ans, abs(y2-y1));
-	ans = max(ans, abs(z2-z1));
-	return ans;
-}
-vector<vector<coord>> permute(vector<coord>& base){
-	vector<vector<coord>> out(24, vector<coord>(base.size()));
-	for(int j = 0;j<base.size();j++){
-		auto [x,y,z] = base[j];
-		out[0][j] = coord(+x, +y, +z);
-		out[1][j] = coord(+y, +z, +x);
-		out[2][j] = coord(+z, +x, +y);
-		out[3][j] = coord(+z, +y, -x);
-		out[4][j] = coord(+y, +x, -z);
-		out[5][j] = coord(+x, +z, -y);
-		out[6][j] = coord(+x, -y, -z);
-		out[7][j] = coord(+y, -z, -x);
-		out[8][j] = coord(+z, -x, -y);
-		out[9][j] = coord(+z, -y, +x);
-		out[10][j] = coord(+y, -x, +z);
-		out[11][j] = coord(+x, -z, +y);
-		out[12][j] = coord(-x, +y, -z);
-		out[13][j] = coord(-y, +z, -x);
-		out[14][j] = coord(-z, +x, -y);
-		out[15][j] = coord(-z, +y, +x);
-		out[16][j] = coord(-y, +x, +z);
-		out[17][j] = coord(-x, +z, +y);
-		out[18][j] = coord(-x, -y, +z);
-		out[19][j] = coord(-y, -z, +x);
-		out[20][j] = coord(-z, -x, +y);
-		out[21][j] = coord(-z, -y, -x);
-		out[22][j] = coord(-y, -x, -z);
-		out[23][j] = coord(-x, -z, -y);
+vector<point> permutePoints(vector<point>& points, function<point(point&)>& permutation){
+	vector<point> t;
+	for(auto& p: points){
+		t.push_back(permutation(p));
 	}
+	return t;
+}
 
-	
+void genFingerprints(vector<Scanner>& scns){
+	for(auto& s: scns){
+		for(int i = 0;i<s.points.size()-1;i++){
+			for(int j = i+1;j<s.points.size();j++){
+				int fp = dist(s.points[j],s.points[i]);
+				s.fpmap[fp].push_back(s.points[j]);
+				s.fpmap[fp].push_back(s.points[i]);
+				s.fps.insert(fp);
+			}
+		}
+	}
+}
+
+vector<int> sharedPoints(set<int>& a, set<int>& b){
+	vector<int> out;
+	set_intersection(begin(a), end(a), begin(b), end(b), back_insert_iterator(out));
 	return out;
 }
 
-vector<fp> sharedFingerprints(const Scanner& a, const Scanner& b){
-	vector<fp> fps;
-	set_intersection(begin(a.fingerprints), end(a.fingerprints), begin(b.fingerprints), end(b.fingerprints), back_inserter(fps));
-	return fps;
+pair<point, int> maxValue(map<point, int>& m){
+	pair<point, int> p;
+	for(auto& [k,v]: m)
+		if(v>p.second)
+			p = pair<point, int>(k,v);
+	return p;
 }
 
 chrono::time_point<std::chrono::steady_clock> day19(input_t& inp){
 	int p1(0), p2(0);
-	vector<Scanner> scanners;
+
+	vector<function<point(point&)>> perms{
+		[](const point& p) { return point(p.x, p.y, p.z); },
+		[](const point& p) { return point(p.x, -p.z, p.y); },
+		[](const point& p) { return point(p.x, -p.y, -p.z); },
+		[](const point& p) { return point(p.x, p.z, -p.y); },
+		[](const point& p) { return point(-p.y, p.x, p.z); },
+		[](const point& p) { return point(p.z, p.x, p.y); },
+		[](const point& p) { return point(p.y, p.x, -p.z); },
+		[](const point& p) { return point(-p.z, p.x, -p.y); },
+		[](const point& p) { return point(-p.x, -p.y, p.z); },
+		[](const point& p) { return point(-p.x, -p.z, -p.y); },
+		[](const point& p) { return point(-p.x, p.y, -p.z); },
+		[](const point& p) { return point(-p.x, p.z, p.y); },
+		[](const point& p) { return point(p.y, -p.x, p.z); },
+		[](const point& p) { return point(p.z, -p.x, -p.y); },
+		[](const point& p) { return point(-p.y, -p.x, -p.z); },
+		[](const point& p) { return point(-p.z, -p.x, p.y); },
+		[](const point& p) { return point(-p.z, p.y, p.x); },
+		[](const point& p) { return point(p.y, p.z, p.x); },
+		[](const point& p) { return point(p.z, -p.y, p.x); },
+		[](const point& p) { return point(-p.y,-p.z, p.x); },
+		[](const point& p) { return point(-p.z, -p.y, -p.x); },
+		[](const point& p) { return point(-p.y, p.z, -p.x); },
+		[](const point& p) { return point(p.z, p.y, -p.x); },
+		[](const point& p) { return point(p.y, -p.z, -p.x); }
+	};	
+	vector<Scanner> scns;
 	for(auto& l: inp){
 		if(l.empty()) continue;
-		if(l[1] == '-'){
-			scanners.emplace_back(l);
-		}else{
+		if(l[1] == '-')
+			scns.emplace_back(l);
+		else{
 			int x, y, z;
 			char tr;
 			stringstream ss(l);
 			ss>>x>>tr>>y>>tr>>z;
-			scanners.back().beacons.push_back(coord(x,y,z));
+			scns.back().points.emplace_back(x,y,z);
 		}
 	}
-	scanners[0].solved = true;
-	scanners[0].pos = coord(0,0,0);
-	for(auto& s: scanners){
-		for(int i = 0;i<s.beacons.size()-1;i++)
-			for(int j = i+1;j<s.beacons.size();j++){
-				s.fingerprints.emplace_back(
-					dist(s.beacons[i], s.beacons[j]),
-					offmin(s.beacons[i], s.beacons[j]),
-					offmax(s.beacons[i], s.beacons[j])
-				);
-				s.fpmap[s.fingerprints.back()] = make_pair(s.beacons[i], s.beacons[j]);
-			}
-		sort(s.fingerprints.begin(), s.fingerprints.end());
-	}
-	for(auto& base: scanners){
-		if(base.solved == false) continue;
-		for(auto& s: scanners){
-			if(s.id!=base.id && s.solved == false){
-				vector<fp> fps = sharedFingerprints(s, base);
-				if(fps.size() >= 66){
-					set<coord> sPoints, bPoints;
-					for(auto& f: fps){
-						coord a, b;
-						tie(a,b) = s.fpmap[f];
-						sPoints.insert(a);
-						sPoints.insert(b);
-						tie(a,b) = base.fpmap[f];
-						bPoints.insert(a);
-						bPoints.insert(b);
-					}
-					vector<coord> sPointsV;
-					for(auto& c: s.beacons){
-						if(sPoints.count(c))
-							sPointsV.push_back(c);
-					}
-					for(auto& perm: permute(sPointsV)){
-						map<coord, int> tCount;
-						for(auto& p1: perm)
-							for(auto& p2: bPoints){
-								tCount[p1-p2]++;
-							}
-						for(auto& [k,v]: tCount){
+	genFingerprints(scns);
+	bool finished = false;
+	while(!finished){
+		finished = true;
+		for(auto& b: scns){
+			if(b.solved == false || b.done) continue;
+			finished = false;
+			b.done = true;
+			for(auto& s: scns){
+				if(s.id != b.id && !s.solved){
+					auto shared = sharedPoints(b.fps, s.fps);
+					if(shared.size()>=66){
+						// assert(shared.size() == 66);
+						for(auto& perm: perms){
+							vector<point> tempPerm = permutePoints(s.points, perm);
+							map<point, int> counter;
+							for(auto& en: b.points)
+								for(auto& st: tempPerm)
+									counter[en-st]++;
+							auto [k, v] = maxValue(counter);
 							if(v>=12){
-								cout<<base.id<<"->"<<s.id<<"	"<<k<<endl;
+								assert(v==12);
 								s.solved = true;
-								s.pos = base.pos + k;
-								s.beacons = perm;
-								cout<<s.pos<<endl;
-								cout<<s.beacons<<endl;
-								exit(0);
-								// goto solvedOne;
+								s.pos = b.pos+k;
+								s.points = tempPerm;
+								break;
 							}
 						}
 					}
-					solvedOne:;
 				}
 			}
 		}
 	}
-	for(auto& s1: scanners){
-		cout<<s1.id<<"	"<<s1.pos<<endl;
-		// for(auto& s2: scanners){
-		// 	cout<<s1.pos<<"	"<<s2.pos<<"	"<<manhattan(s1.pos, s2.pos)<<endl;
-		// 	p2 = max(p2, manhattan(s1.pos, s2.pos));
-		// }
+	set<point> beacons;
+	for(auto& s: scns){
+		for(auto& b: s.points)
+			beacons.insert(b+s.pos);
+		for(auto& s2: scns)
+			p2 = max(p2, manhattan(s2.pos, s.pos));
 	}
+	p1 = beacons.size();
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
