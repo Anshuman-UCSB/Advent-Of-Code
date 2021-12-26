d933c47ca9142bf4e37f6f947b147317824bc003
Anshuman Dash <anshuman@ucsb.edu>
2021-12-26 03:00:29 +0000

    Day 3 optimized with bitsets

--- 2021/c++/src/day03.cpp
+++ 2021/c++/src/day03.cpp
@@ -1,54 +1,49 @@
 #include "AOC.h"
 
 chrono::time_point<std::chrono::steady_clock> day03(input_t& inp){
-	int p1(0), p2(0);
-	int width = inp[0].size();
+	unsigned long p1, p2;
+	const int width = 12;
 
-	int counter;
-	string eps=inp[0], gam=inp[0];
+	vector<bitset<width>> nums;
+	for(auto& v: inp)
+		nums.emplace_back(v);
+	int c;
+	bool res;
+	bitset<width> eps, gam;
 	for(int i = 0;i<width;i++){
-		counter = 0;
-		for(auto& w: inp){
-			counter+=w[i]=='1'?1:-1;
-		}
-		if(counter>=0){
-			eps[i]='1';
-			gam[i]='0';
-		}else{
-			eps[i]='0';
-			gam[i]='1';
-		}
+		c = 0;
+		for(auto& b: nums)
+			c+=b[i];
+		res = c*2>=nums.size();
+		gam[i]=res;
+		eps[i]=!res;
 	}
-	p1 = stoi(eps, 0, 2)* stoi(gam, 0, 2);
-
-	vector<string> oxy(inp), co2(inp);
-	int ox(0), co(0);
-	int oxyc, co2c;
-	for(int i =0;i<width;i++){
-		oxyc = co2c = 0;
-		for(auto& w: oxy)
-			oxyc+=(w[i]=='1'?1:-1);
-		for(auto& w: co2)
-			co2c+=(w[i]=='1'?1:-1);
-		if(!ox){
-			for(int j = oxy.size()-1;j>=0;j--)
-				if(oxy[j][i] != (oxyc>=0?'1':'0')){
-					oxy.erase(oxy.begin()+j);
-				}
-			if(oxy.size() == 1)
-				ox = stoi(oxy[0],0,2);
-		}
-		if(!co){
-			for(int j = co2.size()-1;j>=0;j--)
-				if(co2[j][i] == (co2c>=0?'1':'0'))
-					co2.erase(co2.begin()+j);
-			if(co2.size() == 1)
-				co = stoi(co2[0],0,2);
-		}
-		if(ox && co)
-			break;
+	p1 = gam.to_ulong()*eps.to_ulong();
+	list<bitset<width>> mx, mn;
+	for(auto& b: nums){
+		mx.push_back(b);
+		mn.push_back(b);
 	}
-	p2 = co*ox;
+	eps = gam = bitset<width>();
+	int i = width-1;
+	while(mx.size()>1){
+		c=0;
+		for(auto& b: mx)
+			c+=b[i];
+		res = c*2>=mx.size();
+		mx.remove_if([&](const bitset<width>& b){return b[i]!=res;});
+		i--;
+	}
+	i = width-1;
+	while(mn.size()>1){
+		c=0;
+		for(auto& b: mn)
+			c+=b[i];
+		res = c*2>=mn.size();
+		mn.remove_if([&](const bitset<width>& b){return b[i]==res;});
+		i--;
+	}
+	p2= mx.front().to_ulong()*mn.front().to_ulong();
 	auto done = chrono::steady_clock::now();
 	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
