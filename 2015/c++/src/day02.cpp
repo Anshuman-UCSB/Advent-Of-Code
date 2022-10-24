a879e67c5dae67872ac23d008d924a0c92a7bbc4
Anshuman Dash <anshuman@ucsb.edu>
2022-10-23 22:46:15 -0700

    :)

--- 2015/c++/src/day02.cpp
+++ 2015/c++/src/day02.cpp
@@ -1,8 +1,18 @@
 #include "AOC.h"
 
 chrono::time_point<std::chrono::steady_clock> day02(input_t& inp){
-	
+	int p1(0), p2(0);
+	int l, w, h;
+	char tr;
+	for(auto& line: inp){
+		stringstream ss(line);
+		ss>>l>>tr>>w>>tr>>h;
+		p1+=2*(l*w + w*h + h*l);
+		p1+=min(l*w, min(w*h, h*l));
+		p2+=l*w*h;
+		p2+=2*(l+w+h-max(l,max(w,h)));
+	}
 	auto done = chrono::steady_clock::now();
-	//cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
+	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
 }
--- 2015/c++/src/day03.cpp
+++ 2015/c++/src/day03.cpp
@@ -1,7 +1,14 @@
 #include "AOC.h"
+#include <complex>
 
 chrono::time_point<std::chrono::steady_clock> day03(input_t& inp){
+	set<complex<int>> visited;
+
+	complex<int> base;
+	unordered_map<char, 
 	
+	cout<<base<<endl;
+
 	auto done = chrono::steady_clock::now();
 	//cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
 	return done;
