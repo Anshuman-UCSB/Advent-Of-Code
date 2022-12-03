6ca8b5f1fbb4f0c92aaec6c93ec974ecc64faebe
Anshuman Dash <anshuman@ucsb.edu>
2022-12-02 21:47:20 -0800

    :3

--- 2022/Python/src/day02.py
+++ 2022/Python/src/day02.py
@@ -1,4 +1,27 @@
 from utils.aocUtils import *
+def win(a, b):
+	p1 = ord(a) - ord('A')
+	p2 = ord(b) - ord('X')
+	if p2 == (p1 + 1)%3:
+		return 6
+	if (p2 + 1)%3 == p1:
+		return 0
+	return 3
 def main(input:str):
 	p1 = p2 = 0
+	scores = {'X':1, 'Y':2, 'Z':3}
+	result = {'A':{'X':3,'Y':6,'Z':0},'B':{'X':0,'Y':3,'Z':6},'C':{'X':6,'Y':0,'Z':3}}
+	p2Map = {'X': 0, 'Y': 3, 'Z': 6}
+	for l in input.splitlines():
+		a, b = l.split()
+		p1 += result[a][b]
+		p1 += scores[b]
+		
+		p2 += p2Map[b]
+		for k,v in result[a].items():
+			if v == p2Map[b]:
+				b = k
+				break
+		p2 += scores[b]
+		
 	return (p1, p2)
--- 2022/Python/src/day03.py
+++ 2022/Python/src/day03.py
@@ -1,4 +1,20 @@
 from utils.aocUtils import *
+@cache
+def score(l):
+	return ord(l.lower()) - ord('a') + 1 + l.isupper()*26
 def main(input:str):
 	p1 = p2 = 0
+	c = 0
+	for l in input.splitlines():
+		sz = len(l)//2
+		shared ,= set(l[:sz])&set(l[sz:])
+		p1 += score(shared)
+		if c == 0:
+			st = set(l)
+		else:
+			st &= set(l)
+		if c == 2:
+			sh ,= st
+			p2 += score(sh)
+		c =(c+1)%3
 	return (p1, p2)
