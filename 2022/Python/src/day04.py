420e9514c68d821205aaf46dc55951ff38286fd7
Anshuman Dash <anshuman@ucsb.edu>
2022-12-03 21:16:33 -0800

    Day 4

--- 2022/Python/src/day04.py
+++ 2022/Python/src/day04.py
@@ -1,4 +1,19 @@
 from utils.aocUtils import *
+def toSet(section):
+	a,b = section.split("-")
+	return (int(a), int(b))
+def contains(A,B):
+	return (A[0]<=B[0] and A[1]>=B[1]) or (B[0]<=A[0] and B[1]>=A[1])
+def overlap(A,B):
+	return A[0]<=B[0]<=A[1] or B[0]<=A[0]<=B[1]
 def main(input:str):
 	p1 = p2 = 0
+	for l in input.splitlines():
+		a,b = l.split(",")
+		a = toSet(a)
+		b = toSet(b)
+		if contains(a,b):
+			p1+=1
+		if overlap(a,b):
+			p2+=1
 	return (p1, p2)
