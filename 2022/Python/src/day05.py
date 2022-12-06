456c918b30ca857ab0897559412cc0a05e3c2e25
Anshuman Dash <anshuman@ucsb.edu>
2022-12-05 21:29:26 -0800

    day 6

--- 2022/Python/src/day05.py
+++ 2022/Python/src/day05.py
@@ -1,4 +1,39 @@
 from utils.aocUtils import *
+from copy import deepcopy
+
 def main(input:str):
-	p1 = p2 = 0
+	stacks = [[] for _ in range(9)]
+	inp_part = 0
+	
+	for l in input.splitlines():
+		if l == "":
+			continue
+		if l.startswith(" 1   2 "):
+			inp_part = 1
+			for i in range(len(stacks)):
+				stacks[i] = stacks[i][::-1]
+			stacks2 = deepcopy(stacks)
+		elif inp_part == 0:
+			try:
+				for i in range(9):
+					if l[1+i*4]!=' ':
+						stacks[i].append(l[1+i*4])
+			except IndexError:
+				continue
+		elif inp_part == 1:
+			spl = l.split()
+			cnt, a, b = int(spl[1]), int(spl[3])-1, int(spl[5])-1
+			tmp = []
+			for _ in range(cnt):
+				stacks[b].append(stacks[a].pop(-1))
+				tmp.insert(0, stacks2[a].pop(-1))
+			stacks2[b]+=tmp
+	p1 = ""
+	for s in stacks:
+		if s:
+			p1+=s.pop(-1)
+	p2 = ""
+	for s in stacks2:
+		if s:
+			p2+=s.pop(-1)
 	return (p1, p2)
--- 2022/Python/src/day06.py
+++ 2022/Python/src/day06.py
@@ -1,4 +1,10 @@
 from utils.aocUtils import *
 def main(input:str):
 	p1 = p2 = 0
+	for i in range(4,len(input)):
+		if p1 == 0 and len(set(input[i-4:i])) == 4:
+			p1 = i
+		if len(set(input[i-14:i])) == 14:
+			p2 = i
+			break
 	return (p1, p2)
