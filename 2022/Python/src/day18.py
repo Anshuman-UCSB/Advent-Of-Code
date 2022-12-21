b17eec9bdf864535d201ece016fa8ff5ac1d6c48
Anshuman Dash <anshuman@ucsb.edu>
2022-12-20 19:49:21 -0800

    I stole this code and I made it 6x worse

--- 2022/Python/src/day18.py
+++ 2022/Python/src/day18.py
@@ -1,4 +1,40 @@
 from utils.aocUtils import *
+import numpy as np
 def main(input:str):
-	p1 = p2 = 0
+	p2 = 0
+	points = set()
+	low, high = np.array([float('inf')]*3), np.array([float('-inf')]*3)
+	dirs = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
+	for l in input.splitlines():
+		x,y,z = map(int,(l.split(',')))
+		t = np.array([x,y,z])
+		low = np.minimum(t, low)
+		high = np.maximum(t, high)
+		points.add((x, y, z))
+	low-=1
+	high+=1
+	def inBounds(point):
+		return all(low[i]<=point[i]<=high[i] for i in range(3))
+	
+	def flood(frontier, seen = set()):
+		if len(frontier) == 0:
+			return seen
+		t = set()
+		for x,y,z in frontier:
+			for dx,dy,dz in dirs:
+				tmp = (x+dx,y+dy,z+dz)
+				if inBounds(tmp) and tmp not in points and tmp not in seen:
+					t.add(tmp)
+		seen|=t
+		flood(t, seen)
+		return seen
+	outside = flood(set([tuple(low)]))
+
+	p1 = len(points)*6
+	for x,y,z in points:
+		for dx,dy,dz in dirs:
+			if (x+dx,y+dy,z+dz) in points:
+				p1-=1
+			if (x+dx,y+dy,z+dz) in outside:
+				p2+=1
 	return (p1, p2)
--- 2022/Python/src/day19.py
+++ 2022/Python/src/day19.py
@@ -1,4 +1,45 @@
 from utils.aocUtils import *
+import numpy as np
+import re
+
+def parse(line):
+	n, *nums = list(map(int,re.findall("\d+",line)))
+	costs = np.zeros((4,4),dtype=int)
+	costs[0,0] = nums[0]
+	costs[1,0] = nums[1]
+	costs[2,0:2] = nums[2:4]
+	costs[3,0], costs[3,2] = nums[4], nums[5]
+	return costs
+
 def main(input:str):
-	p1 = p2 = 0
+	p1,p2 = 0,1
+	o = [ sum(range(n)) for n in range(35) ]
+	bps = list(map(parse, input.splitlines()))
+	for n, costs in enumerate(bps):
+		maximal = np.amax(costs, 0)
+		def dfs( t , rb, best,
+				inv,
+				rbs ):
+			if (rb < 3 and rbs[rb]>=maximal[rb]
+				or inv[3] + rbs[3] * t + o[ t ] <= best[0]):
+				return 
+			while t:
+				if all(inv >= costs[rb]):
+					built = np.zeros(4, dtype=int)
+					built[rb] += 1
+					for g in range(4):
+						dfs(t-1, g, best, inv - costs[rb] + rbs, rbs + built)
+					return
+				t -= 1
+				inv += rbs
+			best[0] = max(best[0], inv[3])
+		best = [0]
+		for g in range( 4 ):
+			dfs( 24, g, best, np.zeros(4, dtype=int), np.array([1,0,0,0]))
+		p1 += best[0] * (n+1)
+		if n < 3:
+			best = [0]
+			for g in range( 4 ):
+				dfs( 32, g, best, np.zeros(4, dtype=int), np.array([1,0,0,0]))
+			p2*=best[0]
 	return (p1, p2)
