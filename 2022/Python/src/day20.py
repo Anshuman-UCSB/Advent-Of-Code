5bb659ce433f02b87a40d419c1bfc8d3f3603e0b
Anshuman Dash <anshuman@ucsb.edu>
2022-12-20 22:22:30 -0800

    Brute forced

--- 2022/Python/src/day20.py
+++ 2022/Python/src/day20.py
@@ -2,28 +2,23 @@ from utils.aocUtils import *
 
 def main(input:str):
 	p1 = p2 = 0
-	nums = []
-	
-	for id,l in enumerate(input.splitlines()):
-		nums.append((int(l),id))
-	def getInd(id):
-		for ind, val in enumerate(nums):
-			if val[1] == id:
-				return ind
-		return -1
-	def findZero():
-		for ind, val in enumerate(nums):
-			if val[0] == 0:
-				return ind
-		return -1
-	for id in range(len(nums)):
-		pos = getInd(id)
-		v = nums.pop(pos)
-		pos = (pos+v[0])%len(nums)
-		nums.insert(pos, v)
-	print([v[0] for v in nums])
-	zero_ind = findZero()
-	for di in range(1000,4000,1000):
-		print(p1, zero_ind, di)
-		p1+=nums[(zero_ind+di)%len(nums)][0]
+	inp1 = [*enumerate(int(x) * 1 for x in input.splitlines())]
+	inp2 = [*enumerate(int(x) * 811589153 for x in input.splitlines())]
+	for x in inp1 * 1:
+		inp1.pop(i := inp1.index(x))
+		inp1.insert((i+x[1]) % len(inp1), x)
+	inp1 = [x for _,x in inp1]
+	zind = inp1.index(0)
+	for di in (1000,2000,3000):
+		p1+=inp1[(zind+di)%len(inp1)]
+
+
+	for x in inp2 * 10:
+		inp2.pop(i := inp2.index(x))
+		inp2.insert((i+x[1]) % len(inp2), x)
+	inp2 = [x for _,x in inp2]
+	zind = inp2.index(0)
+	for di in (1000,2000,3000):
+		p2+=inp2[(zind+di)%len(inp2)]
+
 	return (p1, p2)
