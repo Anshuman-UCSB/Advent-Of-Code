c5470e731d134cd2caeb4ad71d9ded4fe6d92fa2
Anshuman Dash <anshuman@ucsb.edu>
2022-12-22 22:42:28 -0800

    Updated with faster code

--- 2022/Python/src/day19.py
+++ 2022/Python/src/day19.py
@@ -1,45 +1,51 @@
 from utils.aocUtils import *
-import numpy as np
 import re
 
+# Full credit to /u/Boojum, pretty much word for word stolen solution; his was just years faster than my numpy version
 def parse(line):
-	n, *nums = list(map(int,re.findall("\d+",line)))
-	costs = np.zeros((4,4),dtype=int)
-	costs[0,0] = nums[0]
-	costs[1,0] = nums[1]
-	costs[2,0:2] = nums[2:4]
-	costs[3,0], costs[3,2] = nums[4], nums[5]
-	return costs
-
+	return list(map(int,re.findall("\d+",line)))
+	
 def main(input:str):
 	p1,p2 = 0,1
 	o = [ sum(range(n)) for n in range(35) ]
 	bps = list(map(parse, input.splitlines()))
-	for n, costs in enumerate(bps):
-		maximal = np.amax(costs, 0)
-		def dfs( t , rb, best,
-				inv,
-				rbs ):
-			if (rb < 3 and rbs[rb]>=maximal[rb]
-				or inv[3] + rbs[3] * t + o[ t ] <= best[0]):
-				return 
+	for n, a,b,c,d,e,f in bps:
+		mi,mj,mk = max(a,b,c,e), d, f
+		def dfs( t, g,best,# t:time remaining, g:goal robot type, best: [bestval]
+			 i, j, k, l,   # i:ore, j:clay, k:obsidian, l:geode robots
+			 w, x, y, z ): # w:ore, x:clay, y:obsidian, z:geode available
+			if ( g == 0 and i >= mi or
+				g == 1 and j >= mj or
+				g == 2 and ( k >= mk or j == 0 ) or
+				g == 3 and k == 0 or
+				z + l * t + o[ t ] <= best[0]):
+				return
 			while t:
-				if all(inv >= costs[rb]):
-					built = np.zeros(4, dtype=int)
-					built[rb] += 1
-					for g in range(4):
-						dfs(t-1, g, best, inv - costs[rb] + rbs, rbs + built)
+				if g == 0 and w >= a:
+					for g in range( 4 ):
+						dfs(t-1, g, best, i + 1, j, k, l, w - a + i, x + j, y + k, z + l )
 					return
-				t -= 1
-				inv += rbs
-			best[0] = max(best[0], inv[3])
+				elif g == 1 and w >= b:
+					for g in range( 4 ):
+						dfs(t-1, g, best, i, j + 1, k, l, w - b + i, x + j, y + k, z + l )
+					return
+				elif g == 2 and w >= c and x >= d:
+					for g in range( 4 ):
+						dfs(t-1, g, best, i, j, k + 1, l, w - c + i, x - d + j, y + k, z + l )
+					return
+				elif g == 3 and w >= e and y >= f:
+					for g in range( 4 ):
+						dfs(t-1, g, best, i, j, k, l + 1, w - e + i, x + j, y - f + k, z + l )
+					return
+				t, w, x, y, z = t - 1, w + i, x + j, y + k, z + l
+			best[0] = max(best[0],z)
 		best = [0]
 		for g in range( 4 ):
-			dfs( 24, g, best, np.zeros(4, dtype=int), np.array([1,0,0,0]))
-		p1 += best[0] * (n+1)
-		if n < 3:
+			dfs( 24, g, best, 1,0,0,0, 0,0,0,0)
+		p1 += best[0] * n
+		if n < 4:
 			best = [0]
 			for g in range( 4 ):
-				dfs( 32, g, best, np.zeros(4, dtype=int), np.array([1,0,0,0]))
+				dfs( 32, g, best, 1,0,0,0, 0,0,0,0)
 			p2*=best[0]
 	return (p1, p2)
--- 2022/Python/src/day23.py
+++ 2022/Python/src/day23.py
@@ -1,4 +1,77 @@
-from utils.aocUtils import *
+from collections import Counter
+P = complex
+unpack = lambda xs: map(int, (xs.real, xs.imag))
+def printGrid(grid,highlight=set()):
+	lx=ly=float('inf')
+	hx=hy=float('-inf')
+	for x,y in map(unpack, grid):
+		lx = min(lx, x)
+		ly = min(ly, y)
+		hx = max(hx, x)
+		hy = max(hy, y)
+	print()
+	for y in range(hy,ly-1,-1):
+		for x in range(lx,hx+1):
+			if P(x,y) in highlight:
+				print('@',end='')
+			else:
+				print('#' if P(x,y) in grid else '.',end='')
+		print()
+
+dirs = {'N': 1j, 'NE': (1+1j), 'NW': (-1+1j), 'S': -1j, 'SE': (1-1j), 'SW': (-1-1j), 'E': 1, 'W': -1}
+def getProposal(grid, elfPos, turn):
+	for dx in dirs.values():
+		if elfPos+dx in grid:
+			break
+	else:
+		return elfPos # no neighbors
+	rules = [
+		(('NW','N','NE'),'N'),
+		(('SW','S','SE'),'S'),
+		(('SW','W','NW'),'W'),
+		(('SE','E','NE'),'E'),
+	]
+	for check, prop in rules[turn:]+rules[:turn]:
+		if all(elfPos+dirs[d] not in grid for d in check):
+			return elfPos+dirs[prop]
+	return elfPos
+
+def iter(grid, turn):
+	proposals = {}
+	c = Counter()
+	for elf in grid:
+		prop = getProposal(grid,elf,turn)
+		proposals[elf] = prop
+		c[prop]+=1
+	n_grid = set()
+	for elf in grid:
+		prop = proposals[elf]
+		n_grid.add(prop if c[prop]==1 else elf)
+	return n_grid
+
+def p1Score(grid):
+	lx=ly=float('inf')
+	hx=hy=float('-inf')
+	for x,y in map(unpack, grid):
+		lx = min(lx, x)
+		ly = min(ly, y)
+		hx = max(hx, x)
+		hy = max(hy, y)
+	return (hy-ly+1)*(hx-lx+1)-len(grid)
+
 def main(input:str):
 	p1 = p2 = 0
-	return (p1, p2)
+	grid = set()
+	for y, row in enumerate(input.splitlines()):
+		for x, v in enumerate(row):
+			if v == '#':
+				grid.add(P(x,-y))
+	for turn in range(10):
+		grid = iter(grid,turn%4)
+	for turn in range(10,99999):
+		old = grid
+		grid = iter(grid,turn%4)
+		if old == grid:
+			p2 = turn+1
+			break
+	return (p1Score(grid), p2)
