66f9cf433716f687bed1f5c57c9d8135842b3b4a
Anshuman Dash <anshuman@ucsb.edu>
2023-02-27 18:42:02 -0800

    Light work

--- 2017/python/day11/main.py
+++ 2017/python/day11/main.py
@@ -1,2 +1,19 @@
 def day11(input):
-	return [None,None]
+	p2=0
+	step={
+		'n':2j,
+		'ne':1+1j,
+		'se':1-1j,
+		's':-2j,
+		'sw':-1-1j,
+		'nw':-1+1j
+	}
+	def dist(p):
+		return int(abs(p.real)+max(abs(p.imag)-abs(p.real),0)//2)
+	p=0
+	for c in input.split(','):
+		p+=step[c]
+		p2=max(p2,dist(p))
+	print(p)
+	p1=dist(p)
+	return [p1,p2]
