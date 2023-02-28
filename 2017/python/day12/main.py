db328c2d191131c58c0b15ae97b31ec1e326d49f
Anshuman Dash <anshuman@ucsb.edu>
2023-02-27 19:23:47 -0800

    LIVING LIKE A WASTE

--- 2017/python/day12/main.py
+++ 2017/python/day12/main.py
@@ -1,2 +1,19 @@
 def day12(input):
-	return [None,None]
+	# UNION FIND MY BELOVED
+	groups = [*range(len(input.splitlines()))]
+	def find(a):
+		if a==groups[a]:
+			return a
+		groups[a]=find(groups[a])
+		return groups[a]
+	def join(a,b):
+		p = find(a)
+		groups[find(b)] = p
+		
+	for l in input.splitlines():
+		lhs,rhs=l.split('<->')
+		for v in map(int,rhs.split(',')):
+			join(int(lhs),v)
+	for i in range(len(groups)):
+		find(i)
+	return [groups.count(groups[0]),len(set(groups))]
