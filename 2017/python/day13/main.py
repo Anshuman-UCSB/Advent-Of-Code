b1d79ec5434f785f7af95f1b9b02a737d2b38a93
Anshuman Dash <anshuman@ucsb.edu>
2023-03-01 22:54:51 -0800

    use CRT

--- 2017/python/day13/main.py
+++ 2017/python/day13/main.py
@@ -1,6 +1,24 @@
+from math import gcd
+def lcm(a,b):
+	return a*b //gcd(a,b)
+
 def day13(input):
 	scn = {}
 	for l in input.splitlines():
-		d,r=map(int,l.split())
+		d,r=map(int,l.split(": "))
 		scn[d]=(r,(r-1)*2)
-	return [None,None]
+	p1=0
+	for i in range(max(scn)+1):
+		if i in scn:
+			if i%scn[i][1] == 0:
+				p1+=i*scn[i][0]
+	p2 = 0
+	while True:
+		p2+=1
+		for i in range(max(scn)+1):
+			if i in scn:
+				if (p2+i)%scn[i][1] == 0:
+					break
+		else:
+			break
+	return [p1,p2]
