d489b5dbda5b6c9d8bfcaa6c6205b05bb596f115
Anshuman Dash <anshuman@ucsb.edu>
2023-03-06 15:14:29 -0800

    progress

--- 2017/python/day17/main.py
+++ 2017/python/day17/main.py
@@ -1,2 +1,18 @@
 def day17(input):
-	return [None,None]
+	ss=int(input)
+	arr=[0]
+	i=0
+	for v in range(1,2018):
+		i=1+((i+ss)%len(arr))
+		arr.insert(i,v)
+		# print(arr,i)
+	p1 = arr[(i+1)%len(arr)]
+
+	l=1
+	i=0
+	for v in range(1,50000000):
+		i=1+((i+ss)%l)
+		l+=1
+		if i==1:
+			p2=v
+	return [p1,p2]
--- 2017/python/day18/main.py
+++ 2017/python/day18/main.py
@@ -1,2 +1,35 @@
+from string import ascii_lowercase
+pos=0
 def day18(input):
+	regs = {l:0 for l in ascii_lowercase}
+	def num(n):
+		try:
+			return int(n)
+		except:
+			return regs[n]
+	def parse(cmd):
+		global pos
+		op,*spl=cmd.split()
+		if op=='snd':
+			pass
+		elif op=='set':
+			regs[spl[0]]=num(spl[1])
+		elif op=='add':
+			regs[spl[0]]+=num(spl[1])
+		elif op=='mul':
+			regs[spl[0]]*=num(spl[1])
+		elif op=='mod':
+			regs[spl[0]]%=num(spl[1])
+		elif op=='rcv':
+			if num(spl[0]):
+				pass # recover?
+		elif op=='jgz':
+			if num(spl[0])>0:
+				pos+=int(spl[1])-1
+		pos+=1
+	cmds=input.splitlines()
+	while 0<=pos<len(cmds):
+		parse(cmds[pos])
+		print(pos)
+	print(regs)
 	return [None,None]
