611202d202f2db1ffef9c22d3b5dbe0e82b6fc13
Anshuman Dash <anshuman@ucsb.edu>
2023-03-06 19:46:59 -0800

    ?" git add .

--- 2017/python/day18/main.py
+++ 2017/python/day18/main.py
@@ -1,17 +1,32 @@
 from string import ascii_lowercase
+from collections import defaultdict,deque
 pos=0
-def day18(input):
-	regs = {l:0 for l in ascii_lowercase}
+sound=None
+
+class code:
+	def __init__(self, id):
+		self.regs = defaultdict(int,{"p":id})
+		self.q=deque()
+		self.pos=0
+	def get(self,v):
+		try: return int(v)
+		except: return self.regs[v]
+	def iter(self):
+		 pass
+
+def day18(inp):
+	regs = defaultdict(int)
 	def num(n):
 		try:
 			return int(n)
 		except:
 			return regs[n]
 	def parse(cmd):
-		global pos
+		global pos, sound
 		op,*spl=cmd.split()
 		if op=='snd':
-			pass
+			sound=num(spl[0])
+			print("playing sound",sound)
 		elif op=='set':
 			regs[spl[0]]=num(spl[1])
 		elif op=='add':
@@ -22,14 +37,15 @@ def day18(input):
 			regs[spl[0]]%=num(spl[1])
 		elif op=='rcv':
 			if num(spl[0]):
-				pass # recover?
+				return sound
 		elif op=='jgz':
 			if num(spl[0])>0:
 				pos+=int(spl[1])-1
 		pos+=1
-	cmds=input.splitlines()
+	cmds=inp.splitlines()
 	while 0<=pos<len(cmds):
-		parse(cmds[pos])
-		print(pos)
-	print(regs)
-	return [None,None]
+		r = parse(cmds[pos])
+		if r!=None:
+			p1=r
+			break
+	return [p1,None]
