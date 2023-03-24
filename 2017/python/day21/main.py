91c3d3a49e8c57aec17318ad8ba7fc9ba0c46314
Anshuman Dash <anshuman@ucsb.edu>
2023-03-24 16:57:59 -0700

    part 1 done

--- 2017/python/day21/main.py
+++ 2017/python/day21/main.py
@@ -9,7 +9,19 @@ def parse(line):
 def iter(grid, rules):
 	sz = len(grid)
 	if sz % 2 == 0:
-		
+		ngrid = np.ones((sz//2*3,sz//2*3),dtype=bool)
+		for y in range(sz//2):
+			for x in range(sz//2):
+				slc = grid[(y)*2:(y+1)*2,(x)*2:(x+1)*2]
+				ngrid[(y)*3:(y+1)*3,(x)*3:(x+1)*3]=rules[np.array2string(slc)]
+		return ngrid
+	else:
+		ngrid = np.ones((sz//3*4,sz//3*4),dtype=bool)
+		for y in range(sz//3):
+			for x in range(sz//3):
+				slc = grid[(y)*3:(y+1)*3,(x)*3:(x+1)*3]
+				ngrid[(y)*4:(y+1)*4,(x)*4:(x+1)*4]=rules[np.array2string(slc)]
+		return ngrid
 
 def day21(input):
 	# grid = np.array([[0,1,0],[0,0,1],[1,1,1]],dtype=bool)
@@ -17,11 +29,15 @@ def day21(input):
 	rules = {}
 	for l in input.splitlines():
 		lhs,rhs = map(parse,l.split(" => "))
-		print(lhs)
 		for _ in range(2):
 			for _ in range(4):
 				lhs = np.rot90(lhs)
 				rules[np.array2string(lhs)]=rhs
 			lhs = np.fliplr(lhs)
-	print(rules)
-	return [None,None]
+	for _ in range(5):
+		grid = iter(grid,rules)
+	p1 = np.sum(grid)
+	for _ in range(18-5):
+		grid = iter(grid,rules)
+	p2 = np.sum(grid)
+	return [p1,p2]
--- 2017/python/day22/main.py
+++ 2017/python/day22/main.py
@@ -1,2 +1,26 @@
 def day22(input):
-	return [None,None]
+	input = input.splitlines()
+	y = len(input)
+	x = len(input[0])
+	pos = x//2 - y//2 * 1j
+	grid = set()
+	for j,row in enumerate(input):
+		for i,v in enumerate(row):
+			if v == '#':
+				grid.add(i-j*1j)
+	d = 1j
+
+	infect = 0
+	for _ in range(10000):
+		if pos in grid:
+			d *= -1j
+			grid.remove(pos)
+		else:
+			d *= 1j
+			grid.add(pos)
+			infect+=1
+		pos+=d
+		# print(grid)
+		
+
+	return [infect,None]
