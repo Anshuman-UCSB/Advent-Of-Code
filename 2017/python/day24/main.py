06312dfd52b5662a29dafe850b703721c8c2e48a
Anshuman Dash <anshuman@ucsb.edu>
2023-03-24 22:13:50 -0700

    done.

--- 2017/python/day24/main.py
+++ 2017/python/day24/main.py
@@ -1,2 +1,31 @@
+from collections import defaultdict
 def day24(input):
-	return [None,None]
+	parts = []
+	for l in input.splitlines():
+		parts.append(tuple(map(int,l.split('/'))))
+	possibles = defaultdict(list)
+	for id,(a,b) in enumerate(parts):
+		possibles[a].append(id)
+		possibles[b].append(id)
+
+	p1 = -1
+	p2len = 0
+	p2 = -1
+	def dfs(need, containing):
+		nonlocal p1,p2,p2len
+		continued = False
+		for potential_id in possibles[need]:
+			if potential_id not in containing:
+				dfs(parts[potential_id][0 if parts[potential_id][1]==need else 1],containing|{potential_id})
+				continued = True
+		if not continued:
+			strength = 0
+			for id in containing:
+				strength+=sum(parts[id])
+			p1 = max(p1, strength)
+			if len(containing)>p2len or (len(containing)==p2len and strength > p2):
+				p2len = len(containing)
+				p2 = strength
+		# print("valid bridge of", containing)
+	dfs(0,set())
+	return [p1,p2]
--- 2017/python/day25/main.py
+++ 2017/python/day25/main.py
@@ -1,2 +1,44 @@
+import re
+
+"""
+In state A:
+  If the current value is 0:
+    - Write the value 1.
+    - Move one slot to the right.
+    - Continue with state B.
+  If the current value is 1:
+    - Write the value 0.
+    - Move one slot to the left.
+    - Continue with state C.
+"""
+def parseState(state):
+	spl = state.splitlines()
+	v1 = int(spl[2][-2])
+	d1 = 1 if spl[3][-3] == 'h' else -1
+	n1 = ord(spl[4][-2])-ord("A")
+	v2 = int(spl[6][-2])
+	d2 = 1 if spl[7][-3] == 'h' else -1
+	n2 = ord(spl[8][-2])-ord("A")
+	return ((v1,d1,n1),(v2,d2,n2))
+
 def day25(input):
-	return [None,None]
+	init,*states = input.split("\n\n")
+	states = [parseState(st) for st in states]
+	s = ord(init[15])-ord("A")
+	steps = int(re.search(r"\d+",init).group(0))
+
+	ticker = set()
+	pos = 0
+	for _ in range(steps):
+		st = states[s]
+		v,d,n=st[pos in ticker]
+		if v:
+			ticker|={pos}
+		else:
+			ticker-={pos}
+		pos+=d
+		s = n
+		# print(st)
+		# print(s,pos,ticker)
+
+	return [len(ticker),None]
