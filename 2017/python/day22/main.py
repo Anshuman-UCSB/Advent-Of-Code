a032cde416feaa7773c2f60a44a2a8c15a593927
Anshuman Dash <anshuman@ucsb.edu>
2023-03-24 19:18:03 -0700

    time for manual looking!

--- 2017/python/day22/main.py
+++ 2017/python/day22/main.py
@@ -1,13 +1,16 @@
+from collections import defaultdict
 def day22(input):
 	input = input.splitlines()
 	y = len(input)
 	x = len(input[0])
 	pos = x//2 - y//2 * 1j
 	grid = set()
+	grid2 = defaultdict(int)
 	for j,row in enumerate(input):
 		for i,v in enumerate(row):
 			if v == '#':
 				grid.add(i-j*1j)
+				grid2[i-j*1j] = 2
 	d = 1j
 
 	infect = 0
@@ -22,5 +25,18 @@ def day22(input):
 		pos+=d
 		# print(grid)
 		
+	pos = x//2 - y//2 * 1j
+	d = 1j
 
-	return [infect,None]
+	infect2 = 0
+	dd = [1j, 1, -1j, -1]
+	for _ in range(10000000):
+	# for _ in range(100):
+		d*=dd[grid2[pos]]
+		grid2[pos]=(grid2[pos]+1)%4
+		if grid2[pos] == 2:
+			infect2+=1
+		pos+=d
+		# print(grid2)
+
+	return [infect,infect2]
--- 2017/python/day23/main.py
+++ 2017/python/day23/main.py
@@ -1,2 +1,44 @@
-def day23(input):
-	return [None,None]
+import string
+
+def get(a, regs):
+	if a in string.ascii_lowercase:
+		return regs[a]
+	else:
+		return int(a)
+
+p1 = 0
+def exec(instr, regs):
+	global p1
+	op,a,b = instr.split()
+	# a=get(a,regs)
+	# b=get(b,regs)
+	if op == "set":
+		regs[a]=get(b,regs)
+	elif op == "sub":
+		regs[a]-=get(b,regs)
+	elif op == "mul":
+		regs[a]*=get(b,regs)
+		p1+=1
+	elif op == "jnz":
+		if get(a,regs):
+			return get(b,regs)
+	return 1
+
+def day23(inp):
+	regs = {k:0 for k in "abcdefgh"}
+	p = 0
+	instrs = inp.splitlines()
+	while 0<=p<len(instrs):
+		print(instrs[p],end="  \t")
+		p+=exec(instrs[p],regs)
+		for k,v in regs.items():
+			print(f"{k}[{v}]",end='\t')
+		input()
+
+	# p2
+	p = 0
+	regs = {k:0 for k in "abcdefgh"}
+	regs['a']=1
+	while 0<=p<len(instrs):
+		p+=exec(instrs[p],regs)
+	return [p1,regs['h']]
