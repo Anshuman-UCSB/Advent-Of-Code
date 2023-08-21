f768234b73ec14c70169eb232de5434a21ad0502
Anshuman Dash <anshuman@ucsb.edu>
2023-08-20 19:19:35 -0700

    fastest yet!

--- 2020/python_v2/src/day19.py
+++ 2020/python_v2/src/day19.py
@@ -1,75 +1,34 @@
-from itertools import product
+import re
 
-def expand(rules:dict, cache:dict, limit):
-	while '0' not in cache:
-		for num,rule in rules.items():
-			if num in cache: continue
-			cacheable = True
-			for r in rule:
-				if all([x in cache for x in r]) == False:
-					cacheable = False
-					break
-			if cacheable:
-				solution = set()
-				for r in rule:
-					tmp = []
-					for tok in r:
-						tmp.append(cache[tok])
-					print(tmp, rule)
-					for combo in product(*tmp):
-						to_add = "".join(combo)
-						if len(to_add)<=limit:
-							solution.add(to_add)
-				cache[num] = solution
-def main(input):
-	global solved
-	solved = {}
-	rules_raw,messages = input.split('\n\n')
-	messages = set(messages.splitlines())
-	limit = max(map(len, messages))
-	assert len(messages) == len(input.split('\n\n')[1].splitlines())
-	rules = {}
+def solve(rules, words, part2=False):
 	cache = {}
-	cache2 = {}
-	for l in rules_raw.splitlines():
-		if '"' in l:
-			rules[l.split(':')[0]] = [l.split(':')[1][2]]
-			cache[l.split(':')[0]] = (l.split(':')[1][2],)
-			cache2[l.split(':')[0]] = (l.split(':')[1][2],)
+	parsed = {}
+	requires = {}
+	for l in rules:
+		lhs, rhs = l.split(": ")
+		if '"' in rhs:
+			cache[lhs] = rhs[1]
 		else:
-			rules[l.split(':')[0]] = list(map(lambda x:x.split(),l.split(':')[1].split('|')))
-	expand(rules, cache, limit)
-	p1 = len(set(messages) & cache['0'])
-	print(rules['1'])
-	rules['11'] = [['42']*n + ['31']*n for n in range(1,3)]
-	print(rules['11'])
-	expand(rules, cache2, limit)
-	p2 = len(set(messages) & cache2['0'])
-	return p1, p2
+			parsed[lhs] = [x.split() for x in rhs.split(' | ')]
+			requires[lhs] = set()
+			for pos in parsed[lhs]:
+				requires[lhs].update(pos)
+	if part2:
+		parsed['11'] = [['42']*n+['31']*n for n in range(1,6)]
+	while '0' not in cache:
+		for tok,req in requires.items():
+			if tok in cache:continue
+			if req <= cache.keys():
+				tmp = []
+				for rule in parsed[tok]:
+					# tmp.append("".join([f"({cache[t]})" for t in rule]))
+					tmp.append("".join([f"{cache[t]}" for t in rule]))
+				cache[tok] = "("+"|".join(tmp)+")"
+				if part2 and tok == '8':
+					cache['8']+='+'
+	regex = re.compile("^"+cache['0']+"$")
+	return sum([regex.match(l) is not None for l in words])
 
-# 102
-# 318
-# def replace(tokenized, o, n):
-# 	return [n if x == o else x for x in tokenized]
-
-# def expand(rule:str, rules:dict, limit):
-# 	# if rule in solved:
-# 	# 	print("cached in solved:", solved)
-# 	# 	return solved[rule]
-# 	if len(rule.split())>limit:
-# 		return set()
-# 	if set(rule) == {'a','b',' '}:
-# 		solved[rule] = {rule}
-# 		return {rule}
-# 	else:
-# 		rep = None
-# 		for token in rule.split():
-# 			if token not in "ab":
-# 				rep = token
-# 				break
-# 		generated = {rereplace(rule,token, r) for r in rules[token]}
-# 		out = set()
-# 		for g in generated:
-# 			out|=expand(g, rules,limit)
-# 		solved[rule] = out
-# 		return out
+def main(input):
+	rules, words = map(lambda x: x.splitlines(),input.split('\n\n'))
+	return solve(rules, words),solve(rules, words, True)
