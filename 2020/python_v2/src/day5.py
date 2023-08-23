596971de5195712917e9d2c26fb8ef1b893dbdfa
Anshuman Dash <anshuman@ucsb.edu>
2023-08-22 20:47:48 -0700

    cleaner day 5

--- 2020/python_v2/src/day5.py
+++ 2020/python_v2/src/day5.py
@@ -1,6 +1,6 @@
 def parse(seat):
-	row = int("".join(['1' if l == 'B' else '0' for l in seat[:7]]),2)
-	col = int("".join(['1' if l == 'R' else '0' for l in seat[7:]]),2)
+	row = int(seat[:7].replace('B','1').replace('F','0'),2)
+	col = int(seat[7:].replace('R','1').replace('L','0'),2)
 	return row*8+col	
 	
 def main(input):
