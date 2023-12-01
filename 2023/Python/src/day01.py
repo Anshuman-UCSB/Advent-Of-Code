e1534c31ef744c3dceb83515a531e36167a24cf1
Anshuman Dash <anshuman@ucsb.edu>
2023-11-30 21:55:36 -0800

    day 1 tutorial recorded

--- 2023/Python/src/day01.py
+++ 2023/Python/src/day01.py
@@ -1,27 +1,24 @@
+digit_names = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 def main(input):
 	lines = input.splitlines()
-	total = 0
-	total_2 = 0
+	total1 = 0
+	total2 = 0
 	for line in lines:
+		# digits = []
+		# for char in line:
+		# 	if char.isnumeric():
+		# 		digits.append(char)
 		digits = [char for char in line if char.isnumeric()]
 		if digits:
-			total += int(digits[0]+digits[-1])
+			total1 += int(digits[0]+digits[-1])
 
 		digits = [char for char in translate(line) if char.isnumeric()]
-		total_2 += int(digits[0]+digits[-1])
-	print(translate("eightwo2twothree"))
-	return total, total_2
+		total2 += int(digits[0]+digits[-1])
+
+	return total1, total2
 
 def translate(line):
-	return (line
-		.replace("one", "one1one")
-		.replace("two", "two2two")
-		.replace("three", "three3three")
-		.replace("four", "four4four")
-		.replace("five", "five5five")
-		.replace("six", "six6six")
-		.replace("seven", "seven7seven")
-		.replace("eight", "eight8eight")
-		.replace("nine", "nine9nine")
-		.replace("zero", "zero0zero")
-	)
+	for num, name in enumerate(digit_names):
+		line = line.replace(name, f"{name}{num}{name}")
+	return line
