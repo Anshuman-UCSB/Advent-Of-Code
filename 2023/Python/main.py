d4b83213d1f6f8b6f4287d407c49c4031f6433ff
Anshuman Dash <anshuman@ucsb.edu>
2023-11-30 21:33:56 -0800

    ugly solve

--- 2023/Python/main.py
+++ 2023/Python/main.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 import importlib
 import argparse
 import time
@@ -7,14 +8,25 @@ days = [None]+[importlib.import_module(f"src.day{i:02d}") for i in range(1,26)]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("day", nargs="?", type=int, help="Select just one day to run")
-parser.add_argument("-t", "--test", action="store_true", help="Run tests")
+parser.add_argument("-t", "--test", action="store_true", help="Run tests instead of input")
+parser.add_argument("-d", "--debug", action="store_true", help="Include error outputs for test cases")
 
+args = parser.parse_args()
+if args.debug:
+	args.test = True
+
+	
 def runDay(day, path=None):
 	path = path or f"data/day{day:02d}/input"
 	with open(path,'r') as f:
-		inp = f.read()
+		inp = f.read().strip()
 		start_time = time.time()
-		results = days[day].main(inp)
+		try:
+			results = days[day].main(inp)
+		except Exception as e:
+			if args.debug:
+				traceback.print_exc()
+			results = (str(e),None)
 		elapsed = time.time() - start_time
 		return results, elapsed*1000
 
@@ -32,15 +44,15 @@ def runTests(day):
 	test_paths = os.listdir(f"data/day{day:02d}/tests")
 	for test_path in test_paths:
 		printDay(f"Test {test_path}:", *runDay(day, f"data/day{day:02d}/tests/"+test_path))
+		print()
 
-	with open(f"data/day{day:02d}/input",'r') as f:
-		inp = f.read()
-		start_time = time.time()
-		results = days[day].main(inp)
-		elapsed = time.time() - start_time
-		return results, elapsed*1000
+	# with open(f"data/day{day:02d}/input",'r') as f:
+	# 	inp = f.read().strip()
+	# 	start_time = time.time()
+	# 	results = days[day].main(inp)
+	# 	elapsed = time.time() - start_time
+	# 	return results, elapsed*1000
 
-args = parser.parse_args()
 if args.day == None:
 	print(" -- ADVENT OF CODE 2023 --")
 	if args.test: print("(test flag ignored due to running all days)")
@@ -55,4 +67,5 @@ if args.day == None:
 else:
 	if args.test:
 		runTests(args.day)
-	printDay(f"Day {args.day}:", *runDay(args.day))
+	else:
+		printDay(f"Day {args.day}:", *runDay(args.day))
--- 2023/Python/src/day01.py
+++ 2023/Python/src/day01.py
@@ -1,2 +1,27 @@
 def main(input):
-	return None, None
+	lines = input.splitlines()
+	total = 0
+	total_2 = 0
+	for line in lines:
+		digits = [char for char in line if char.isnumeric()]
+		if digits:
+			total += int(digits[0]+digits[-1])
+
+		digits = [char for char in translate(line) if char.isnumeric()]
+		total_2 += int(digits[0]+digits[-1])
+	print(translate("eightwo2twothree"))
+	return total, total_2
+
+def translate(line):
+	return (line
+		.replace("one", "one1one")
+		.replace("two", "two2two")
+		.replace("three", "three3three")
+		.replace("four", "four4four")
+		.replace("five", "five5five")
+		.replace("six", "six6six")
+		.replace("seven", "seven7seven")
+		.replace("eight", "eight8eight")
+		.replace("nine", "nine9nine")
+		.replace("zero", "zero0zero")
+	)
