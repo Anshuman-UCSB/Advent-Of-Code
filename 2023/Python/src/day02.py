8617e16bbd4a3229adaf39881354050959d99607
Anshuman Dash <anshuman@ucsb.edu>
2023-12-01 23:41:18 -0800

    day 2 tutorial done

--- 2023/Python/src/day02.py
+++ 2023/Python/src/day02.py
@@ -1,3 +1,48 @@
-def main(input):
+class cubeSet:
+	def __init__(self, r, g, b):
+		self.r = r
+		self.g = g
+		self.b = b
+	def __str__(self):
+		return str((self.r, self.g, self.b))
+	def __repr__(self):
+		return str((self.r, self.g, self.b))
 
-	return None
+	def max(self, other):
+		self.r = max(self.r, other.r)
+		self.g = max(self.g, other.g)
+		self.b = max(self.b, other.b)
+	def isSubset(self, other):
+		return (self.r <= other.r
+			and self.g <= other.g
+			and self.b <= other.b)
+	def power(self):
+		return self.r * self.g * self.b
+	
+def parse(reveal):
+	colors = ["red", "green", "blue"]
+	values = [0]*3
+	for token in reveal.split(","):
+		for i, color in enumerate(colors):
+			if color in token:
+				values[i] += int(token.split()[0])
+	return cubeSet(*values)
+
+def main(input):
+	total = cubeSet(12, 13, 14)
+	p1 = p2 = 0
+	for game_num, line in enumerate(input.splitlines()):
+		game_num+=1
+		_, rhs = line.split(":")
+		sets = []
+		for reveal in rhs.split(";"):
+			sets.append(parse(reveal))
+		if all(s.isSubset(total) for s in sets):
+			p1 += game_num
+
+		maxCubes = cubeSet(0,0,0)
+		for s in sets:
+			maxCubes.max(s)
+		p2 += maxCubes.power()
+		
+	return p1, p2
