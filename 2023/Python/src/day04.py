4e20e66c696da39ae30be06333f4dc6888bbecec
Anshuman Dash <anshuman@ucsb.edu>
2023-12-04 01:58:25 -0800

    cute little dp

--- 2023/Python/src/day04.py
+++ 2023/Python/src/day04.py
@@ -1,3 +1,14 @@
-def main(input):
+def matches(l):
+	_, l = l.split(":")
+	lhs, rhs = l.split("|")
+	return len(set(lhs.split()) & set(rhs.split()))
 
-	return None
+def main(input):
+	p1 = 0
+	cards = [1]*len(input.splitlines())
+	for c,l in enumerate(input.splitlines()):
+		num_matches = matches(l)
+		p1 += int(2**(num_matches-1))
+		for i in range(num_matches):
+			cards[c+i+1] += cards[c]
+	return p1, sum(cards)
