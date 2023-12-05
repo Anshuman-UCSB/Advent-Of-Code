7b303e5aafed15554992f76c0595e2c678124606
Anshuman Dash <anshuman@ucsb.edu>
2023-12-05 00:56:46 -0800

    tutorial version uploaded

--- 2023/Python/src/day05.py
+++ 2023/Python/src/day05.py
@@ -1,15 +1,17 @@
-# P2 inspired by /u/Wayoshi
-
 class Range:
 	def __init__(self, lower, upper):
 		self.lower = lower
 		self.upper = upper
+	def __repr__(self):
+		return f"[{self.lower}, {self.upper})"
 	def intersection(self, other):
 		tmp = Range(max(self.lower, other.lower), min(self.upper, other.upper))
 		return tmp if tmp.lower < tmp.upper else None
 	def subtract(self, other):
 		ins = self.intersection(other)
-		if ins == None:	# No intersection, so no change
+		if ins == None:
+			# ----------
+			#    			---
 			return [Range(self.lower, self.upper)]
 		elif (ins.lower, ins.upper) == (self.lower, self.upper):
 			#    -----		intersect
@@ -25,12 +27,10 @@ class Range:
 			return [Range(self.lower, ins.lower)]
 		else:
 			#      --		intersect
-			#    -----		self
+			#    ------		self
 			return [Range(self.lower, ins.lower), Range(ins.upper, self.upper)]
 	def add(self, offset):
-		return Range(self.lower+offset, self.upper+offset)
-	def __repr__(self):
-		return f"[{self.lower}, {self.upper})"
+		return Range(self.lower + offset, self.upper + offset)
 
 class Map:
 	def __init__(self, map_str):
@@ -40,7 +40,7 @@ class Map:
 			self.rules.append((dest, source, size))
 	def convert(self, inp):
 		for dest, source, size in self.rules:
-			if source<=inp<source+size:
+			if source <= inp < source + size:
 				return dest + inp - source
 		return inp
 
@@ -51,37 +51,33 @@ def getLocation(seed, maps):
 
 class p2solver:
 	def __init__(self, maps):
-		self.final_ranges = []
-		self.answer = float('inf')
 		self.maps = maps
-	def propogate(self, r: Range, layer: int):
+		self.answer = float('inf')
+	def propagate(self, r: Range, layer: int):
 		if layer == len(self.maps):
-			self.final_ranges.append(r)
 			self.answer = min(self.answer, r.lower)
 			return
 		for dest, source, size in self.maps[layer].rules:
 			map_r = Range(source, source+size)
 			ins = r.intersection(map_r)
 			if ins is not None:
-				self.propogate(ins.add(dest-source), layer+1)
-				sub = r.subtract(ins)
+				self.propagate(ins.add(dest-source), layer+1)
+				sub = r.subtract(map_r)
 				if len(sub) == 0:
 					return
 				r = sub[0]
 				if len(sub) == 2:
-					self.propogate(sub[1], layer)
-		self.propogate(r, layer+1)
-
-def solve(seeds, maps):
-	locations = [getLocation(s, maps) for s in seeds]
-	return min(locations)
+					self.propagate(sub[1], layer)
+		self.propagate(r, layer+1)
 
 def main(input):
-	seeds, *maps = input.split("\n\n")
+	seeds, *map_strs = input.split("\n\n")
 	seeds = list(map(int, seeds.split()[1:]))
-	maps = list(map(Map, maps))
+	maps = [Map(map_str) for map_str in map_strs]
+	
+	locations = [getLocation(s,maps) for s in seeds]
 	p2 = p2solver(maps)
-	for i in range(0,len(seeds),2):
-		p2.propogate(Range(seeds[i], seeds[i]+seeds[i+1]), 0)
 
-	return solve(seeds, maps), p2.answer
+	for i in range(0,len(seeds),2):
+		p2.propagate(Range(seeds[i], seeds[i]+seeds[i+1]), 0)
+	return min(locations), p2.answer
