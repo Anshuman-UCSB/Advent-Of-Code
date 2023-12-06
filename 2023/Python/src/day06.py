a8b2eae94f61414de5ae300bcc02d0233c936a69
Anshuman Dash <anshuman@ucsb.edu>
2023-12-05 23:43:24 -0800

    even though you smelled like cigarettes, shout out my 7th grade math teacher for this day

--- 2023/Python/src/day06.py
+++ 2023/Python/src/day06.py
@@ -1,3 +1,24 @@
-def main(input):
+import math
+import re
+# time 7, dist 9
+# x - how long we charge our boat up for (also its speed)
+# (7-x)x = 9
+# (t-x)x = d
+# -x^2 + xt = d
+# 0 = x^2 - tx + d
+def findMargin(t, d):
+	d += 1e-6 # magic
+	low = int((t - math.sqrt(t**2 - 4*d))/2)
+	high= int((t + math.sqrt(t**2 - 4*d))/2)
+	return high - low
 
-	return None
+def solve(input):
+	nums = list(map(int, re.findall(r"\d+", input)))
+	num_races = len(nums)//2
+	ans = 1
+	for i in range(num_races):
+		ans*=findMargin(nums[i], nums[i+num_races])
+	return ans
+	
+def main(input):
+	return solve(input), solve(input.replace(" ",""))
