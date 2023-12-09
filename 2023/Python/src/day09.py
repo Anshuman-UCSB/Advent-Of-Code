d43c0b1b62946c621ae664bbd25a2882331972a1
Anshuman Dash <anshuman@ucsb.edu>
2023-12-09 01:07:28 -0800

    bit wonky, also 1am

--- 2023/Python/src/day09.py
+++ 2023/Python/src/day09.py
@@ -1,3 +1,11 @@
-def main(input):
+def solve(nums):
+	if len(set(nums)) == 1:
+		return nums[0]
+	diff = []
+	for i in range(len(nums)-1):
+		diff.append(nums[i+1]-nums[i])
+	return nums[-1] + solve(diff)
 
-	return None
+def main(input):
+	inpNums = [list(map(int, l.split())) for l in input.splitlines()]
+	return sum(solve(n) for n in inpNums), sum(solve(n[::-1]) for n in inpNums)
