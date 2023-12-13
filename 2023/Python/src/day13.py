b9ae217eecf1138c04c7107fb774c531344788d2
Anshuman Dash <anshuman@ucsb.edu>
2023-12-12 22:43:30 -0800

    solved

--- 2023/Python/src/day13.py
+++ 2023/Python/src/day13.py
@@ -1,3 +1,44 @@
-def main(input):
+P = complex
 
-	return None
+def isHorizFlip(grid, maxX, x, part2 = False):
+	flag = False
+	for p in grid:
+		newP = P(x+(x+1-p.real), p.imag)
+		if 0 <= newP.real <= maxX and newP not in grid:
+			if not part2 or (part2 and flag):
+				return False
+			flag = True
+	return not part2 or flag
+
+def isVertFlip(grid, maxY, y, part2 = False):
+	flag = False
+	for p in grid:
+		newP = P(p.real, y+(y+1-p.imag))
+		if 0 <= newP.imag <= maxY and newP not in grid:
+			if not part2 or (part2 and flag):
+				return False
+			flag = True
+	return not part2 or flag
+	
+def solve(grid, maxX, maxY, part2 = False):
+	for x in range(maxX): # horizontal flip across x:x+1
+		if isHorizFlip(grid, maxX, x, part2):
+			return x+1
+	for y in range(maxY): # vertical flip across x:x+1
+		if isVertFlip(grid, maxY, y, part2):
+			return 100*(y+1)
+
+def parse(raw):
+	maxX=maxY=0
+	grid = set()
+	for y, l in enumerate(raw.splitlines()):
+		maxY = y
+		maxX = len(l)-1
+		for x, v in enumerate(l):
+			if v == '#':
+				grid.add(P(x,y))
+	return grid, maxX, maxY
+
+def main(input):
+	grids = [parse(raw) for raw in input.split("\n\n")]
+	return sum(solve(*g) for g in grids), sum(solve(*g, True) for g in grids)
