1400f8d62d174f413077019e0987be68b8580803
Anshuman Dash <anshuman@ucsb.edu>
2023-12-13 22:11:18 -0800

    video version

--- 2023/Python/src/day14.py
+++ 2023/Python/src/day14.py
@@ -1,15 +1,13 @@
-P = complex
-
 def spin(grid):
 	for y in range(len(grid)):
 		for x in range(len(grid[0])):
 			if grid[y][x] == 'O':
 				ny = y
 				grid[ny][x] = '.'
-				while ny>-1 and grid[ny][x] == '.':
+				while ny >= 0 and grid[ny][x] == '.':
 					ny-=1
 				grid[ny+1][x] = 'O'
-	return list(map(list, zip(*grid[::-1])))
+	return list(map(list,zip(*grid[::-1])))
 
 def cycle(grid):
 	for i in range(4):
@@ -23,26 +21,28 @@ def score(grid):
 			if grid[y][x] == 'O':
 				ans += len(grid) - y
 	return ans
-
-def toString(grid):
-	return "\n".join(["".join(l) for l in grid])
+	
 
 def main(input):
 	grid = [list(l) for l in input.splitlines()]
-	spun = spin(grid)
+	# print(*grid, sep='\n')
+	spin(grid)
 	p1 = score(grid)
-	answers = {}
+	
+	seen = {}
+	ff = False
+
 	t = 0
 	target = 1000000000
-	ff = False
 	while t < target:
 		grid = cycle(grid)
-		t+=1
-		h = toString(grid)
-		if not ff and h in answers:
-			print("Found a cycle", t,answers[h])
-			period = t - answers[h]
+		t += 1
+		h = str(grid)
+		
+		if not ff and h in seen:
+			period = t - seen[h]
 			t += ((target-t)//period)*period
 			ff = True
-		answers[h] = t
+
+		seen[h] = t
 	return p1, score(grid)
