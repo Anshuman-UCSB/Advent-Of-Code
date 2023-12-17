cb8c40f251dd529fcec875e870ab545b4ae20d3f
Anshuman Dash <anshuman@ucsb.edu>
2023-12-16 22:06:48 -0800

    youtube walkthrough

--- 2023/Python/src/day17.py
+++ 2023/Python/src/day17.py
@@ -1,4 +1,4 @@
-import heapq
+import heapq # min heap
 P = complex
 
 class State:
@@ -10,8 +10,6 @@ class State:
 		self.key = (self.p, self.d, self.c)
 	def __lt__(self, other):
 		return (self.loss, self.c)<(other.loss, other.c)
-	def __repr__(self):
-		return str((self.loss, self.p, self.d, self.c))
 
 def main(input):
 	grid = {}
@@ -20,28 +18,29 @@ def main(input):
 			grid[P(x,y)] = int(v)
 	shape = len(input.splitlines())
 	end = P(len(input.splitlines()[0])-1, shape-1)
+	
 	def solve(part1):
 		seen = set()
 		Q = [State(0,0,1,0), State(0,0,1j,0)]
-		heapq.heapify(Q)
 		while Q:
 			s = heapq.heappop(Q)
 			if s.key in seen:
 				continue
 			seen.add(s.key)
 			loss, p, d, c = s.loss, s.p, s.d, s.c
-			if p == end and (part1 or c >= 4):
+
+			if p == end and (part1 or c>=4):
 				return loss
-			if p not in grid:
-				continue
-			if ((part1 and c < 3) or (not part1 and c<10)) and p+d in grid:
-				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, c+1)) # go straight
+			# if p not in grid:
+			# 	continue
+			if ((part1 and c<3) or (not part1 and c<10)) and p+d in grid:
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, c+1))
+			
 			d *= 1j
 			if (part1 or 4<=c) and p+d in grid:
-				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1)) # turn clockwise
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1))
 			d *= -1
 			if (part1 or 4<=c) and p+d in grid:
-				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1)) # turn counterclockwise
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1))
 
-	
 	return solve(True), solve(False)
