2e0e89cebbfc16fb0fac19be29d0fcb7b939ad39
Anshuman Dash <anshuman@ucsb.edu>
2023-12-16 21:55:21 -0800

    solved

--- 2023/Python/src/day17.py
+++ 2023/Python/src/day17.py
@@ -1,3 +1,47 @@
-def main(input):
+import heapq
+P = complex
 
-	return None
+class State:
+	def __init__(self, loss, p, d, c):
+		self.loss = loss
+		self.p = p
+		self.d = d
+		self.c = c
+		self.key = (self.p, self.d, self.c)
+	def __lt__(self, other):
+		return (self.loss, self.c)<(other.loss, other.c)
+	def __repr__(self):
+		return str((self.loss, self.p, self.d, self.c))
+
+def main(input):
+	grid = {}
+	for y, r in enumerate(input.splitlines()):
+		for x,v in enumerate(r):
+			grid[P(x,y)] = int(v)
+	shape = len(input.splitlines())
+	end = P(len(input.splitlines()[0])-1, shape-1)
+	def solve(part1):
+		seen = set()
+		Q = [State(0,0,1,0), State(0,0,1j,0)]
+		heapq.heapify(Q)
+		while Q:
+			s = heapq.heappop(Q)
+			if s.key in seen:
+				continue
+			seen.add(s.key)
+			loss, p, d, c = s.loss, s.p, s.d, s.c
+			if p == end and (part1 or c >= 4):
+				return loss
+			if p not in grid:
+				continue
+			if ((part1 and c < 3) or (not part1 and c<10)) and p+d in grid:
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, c+1)) # go straight
+			d *= 1j
+			if (part1 or 4<=c) and p+d in grid:
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1)) # turn clockwise
+			d *= -1
+			if (part1 or 4<=c) and p+d in grid:
+				heapq.heappush(Q, State(loss+grid[p+d], p+d, d, 1)) # turn counterclockwise
+
+	
+	return solve(True), solve(False)
