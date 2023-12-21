8a7a938295b6d2bd59041351ddd59ac74928f2a0
Anshuman Dash <anshuman@ucsb.edu>
2023-12-20 23:31:42 -0800

    vile solution. also slow eww.

--- 2023/Python/src/day21.py
+++ 2023/Python/src/day21.py
@@ -1,8 +1,8 @@
+import numpy as np
 P = complex
 class Grid:
-	def __init__(self, input, part1):
-		self.part1 = part1
-		self.mX, self.mY = len(input.splitlines()[0]), len(input.splitlines())
+	def __init__(self, input):
+		self.size = len(input.splitlines())
 		self.grid = set()
 		self.positions = set()
 		for y, l in enumerate(input.splitlines()):
@@ -15,15 +15,22 @@ class Grid:
 		newPos = set()
 		for p in self.positions:
 			for d in (1,-1,1j,-1j):
-				if p+d not in self.grid and self.inBounds(p+d):
+				if self.wrap(p+d) not in self.grid:
 					newPos.add(p+d)
 		self.positions = newPos
-	def inBounds(self, p):
-		return 0<=p.real<self.mX and 0<=p.imag<self.mY
+	def wrap(self, p):
+		return P(p.real%self.size, p.imag%self.size)
 
 def main(input):
-	g = Grid(input, True)
-	for _ in range(64):
+	g = Grid(input)
+	X,Y = [0,1,2], []
+	target = (26501365 - 65)//131
+	p1 = None
+	for s in range(65+131*2+1):
+		if s%131 == 65:
+			Y.append(len(g.positions))
+		if s == 64:
+			p1 = len(g.positions)		
 		g.step()
-	print(len(g.positions))
-	return len(g.positions), None
+	poly = np.rint(np.polynomial.polynomial.polyfit(X,Y,2)).astype(int).tolist()
+	return p1, sum(poly[i]*target**i for i in range(3))
