b3bed9fe38f1123e26fc88a41882ebbf57a6177d
Anshuman Dash <anshuman@ucsb.edu>
2023-12-20 23:51:25 -0800

    ugly slow youtube solution

--- 2023/Python/src/day21.py
+++ 2023/Python/src/day21.py
@@ -7,9 +7,9 @@ class Grid:
 		self.positions = set()
 		for y, l in enumerate(input.splitlines()):
 			for x, v in enumerate(l):
-				if v == "#":
+				if v == '#':
 					self.grid.add(P(x,y))
-				elif v == 'S':
+				if v == 'S':
 					self.positions.add(P(x,y))
 	def step(self):
 		newPos = set()
@@ -23,14 +23,14 @@ class Grid:
 
 def main(input):
 	g = Grid(input)
+	# f(x) = how many squares are visited at time 65 + 131*x
 	X,Y = [0,1,2], []
 	target = (26501365 - 65)//131
-	p1 = None
-	for s in range(65+131*2+1):
+	for s in range(65 + 131*2 + 1):
 		if s%131 == 65:
 			Y.append(len(g.positions))
 		if s == 64:
-			p1 = len(g.positions)		
+			p1 = len(g.positions)
 		g.step()
 	poly = np.rint(np.polynomial.polynomial.polyfit(X,Y,2)).astype(int).tolist()
 	return p1, sum(poly[i]*target**i for i in range(3))
