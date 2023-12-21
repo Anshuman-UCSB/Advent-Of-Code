a0176e8bc5da6754cf6e7b37980b262348385c37
Anshuman Dash <anshuman@ucsb.edu>
2023-12-21 11:52:55 -0800

    used BFS instead with parity

--- 2023/Python/src/day21.py
+++ 2023/Python/src/day21.py
@@ -4,20 +4,27 @@ class Grid:
 	def __init__(self, input):
 		self.size = len(input.splitlines())
 		self.grid = set()
-		self.positions = set()
+		self.positions = {}
+		self.frontier = set()
+		self.parity = False
 		for y, l in enumerate(input.splitlines()):
 			for x, v in enumerate(l):
 				if v == '#':
 					self.grid.add(P(x,y))
 				if v == 'S':
-					self.positions.add(P(x,y))
+					self.positions[P(x,y)]=self.parity
+					self.frontier.add(P(x,y))
+	def score(self):
+		return sum(v==self.parity for v in self.positions.values())
 	def step(self):
-		newPos = set()
-		for p in self.positions:
+		newFrontier = set()
+		self.parity = not self.parity
+		for p in self.frontier:
 			for d in (1,-1,1j,-1j):
-				if self.wrap(p+d) not in self.grid:
-					newPos.add(p+d)
-		self.positions = newPos
+				if self.wrap(p+d) not in self.grid and p+d not in self.positions:
+					newFrontier.add(p+d)
+					self.positions[p+d] = self.parity
+		self.frontier = newFrontier
 	def wrap(self, p):
 		return P(p.real%self.size, p.imag%self.size)
 
@@ -28,9 +35,9 @@ def main(input):
 	target = (26501365 - 65)//131
 	for s in range(65 + 131*2 + 1):
 		if s%131 == 65:
-			Y.append(len(g.positions))
+			Y.append(g.score())
 		if s == 64:
-			p1 = len(g.positions)
+			p1 = g.score()
 		g.step()
 	poly = np.rint(np.polynomial.polynomial.polyfit(X,Y,2)).astype(int).tolist()
 	return p1, sum(poly[i]*target**i for i in range(3))
