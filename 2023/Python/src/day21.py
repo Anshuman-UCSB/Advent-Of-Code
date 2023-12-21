78fe0101a275c62f7ea186043a9dd633c23054b9
Anshuman Dash <anshuman@ucsb.edu>
2023-12-20 22:19:38 -0800

    solution part 1

--- 2023/Python/src/day21.py
+++ 2023/Python/src/day21.py
@@ -1,3 +1,29 @@
-def main(input):
+P = complex
+class Grid:
+	def __init__(self, input, part1):
+		self.part1 = part1
+		self.mX, self.mY = len(input.splitlines()[0]), len(input.splitlines())
+		self.grid = set()
+		self.positions = set()
+		for y, l in enumerate(input.splitlines()):
+			for x, v in enumerate(l):
+				if v == "#":
+					self.grid.add(P(x,y))
+				elif v == 'S':
+					self.positions.add(P(x,y))
+	def step(self):
+		newPos = set()
+		for p in self.positions:
+			for d in (1,-1,1j,-1j):
+				if p+d not in self.grid and self.inBounds(p+d):
+					newPos.add(p+d)
+		self.positions = newPos
+	def inBounds(self, p):
+		return 0<=p.real<self.mX and 0<=p.imag<self.mY
 
-	return None
+def main(input):
+	g = Grid(input, True)
+	for _ in range(64):
+		g.step()
+	print(len(g.positions))
+	return len(g.positions), None
