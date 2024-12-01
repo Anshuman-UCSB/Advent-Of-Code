3ff8f892ddb3532c54f32bce19706b1be9b9c5cb
Anshuman Dash <anshuman@ucsb.edu>
2024-12-01 02:30:39 +0000

    c++ template made

---
+++ 2024/c++/src/day07.cpp
@@ -0,0 +1,11 @@
+#include "AOC.h"
+
+chrono::time_point<std::chrono::steady_clock> day07(input_t& inp){
+	int p1(0), p2(0);
+
+	
+	
+	auto done = chrono::steady_clock::now();
+	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
+	return done;
+}
