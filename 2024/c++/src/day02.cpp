0ba4243b0be2347fa12ffa2ba8144ecc798aa29f
Anshuman Dash <anshuman@ucsb.edu>
2024-12-02 11:43:54 +0000

    darn

--- 2024/c++/src/day02.cpp
+++ 2024/c++/src/day02.cpp
@@ -1,11 +1,39 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day02(input_t& inp){
+chrono::time_point<std::chrono::steady_clock> day02(input_t &inp)
+{
 	int p1(0), p2(0);
 
-	
-	
+	for (auto &line : inp)
+	{
+		istringstream iss(line);
+		vector<int> nums;
+		int num;
+		while (iss >> num)
+			nums.push_back(num);
+		bool increasing;
+		bool valid = true;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (i == 0)
+			{
+				increasing = nums[i + 1] > nums[i];
+			}
+			else
+			{
+				if (increasing && nums[i - 1] >= nums[i] || ((abs(nums[i - 1] - nums[i]) != 1) && (abs(nums[i - 1] - nums[i] != 3))))
+				{
+					valid = false;
+					break;
+				}
+			}
+		}
+		p1 += valid;
+		if (valid)
+			cout << nums << endl;
+	}
+
 	auto done = chrono::steady_clock::now();
-	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
+	cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
 	return done;
 }
