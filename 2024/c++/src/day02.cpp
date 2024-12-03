de6d64911d261f2464521905c70631616014360b
Anshuman Dash <anshuman@ucsb.edu>
2024-12-03 07:41:49 +0000

    p1 done

--- 2024/c++/src/day02.cpp
+++ 2024/c++/src/day02.cpp
@@ -1,39 +1,33 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day02(input_t &inp)
-{
-	int p1(0), p2(0);
+bool isSafe(const vector<int> &nums, int skip = -1) {
+    int increasing, diff;
+    for (int i = 0; i < nums.size(); i++) {
+        if (i == 0) {
+            increasing = nums[i + 1] > nums[i];
+        } else {
+            diff = nums[i] - nums[i - 1];
+            if (!(diff && (diff > 0 == increasing) && (abs(diff) >= 1) &&
+                  (abs(diff) <= 3))) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
-	for (auto &line : inp)
-	{
-		istringstream iss(line);
-		vector<int> nums;
-		int num;
-		while (iss >> num)
-			nums.push_back(num);
-		bool increasing;
-		bool valid = true;
-		for (int i = 0; i < nums.size(); i++)
-		{
-			if (i == 0)
-			{
-				increasing = nums[i + 1] > nums[i];
-			}
-			else
-			{
-				if (increasing && nums[i - 1] >= nums[i] || ((abs(nums[i - 1] - nums[i]) != 1) && (abs(nums[i - 1] - nums[i] != 3))))
-				{
-					valid = false;
-					break;
-				}
-			}
-		}
-		p1 += valid;
-		if (valid)
-			cout << nums << endl;
-	}
+chrono::time_point<std::chrono::steady_clock> day02(input_t &inp) {
+    int p1(0), p2(0);
 
-	auto done = chrono::steady_clock::now();
-	cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
-	return done;
+    for (auto &line : inp) {
+        istringstream iss(line);
+        vector<int> nums;
+        int num;
+        while (iss >> num) nums.push_back(num);
+        p1 += isSafe(nums);
+    }
+
+    auto done = chrono::steady_clock::now();
+    cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
+    return done;
 }
