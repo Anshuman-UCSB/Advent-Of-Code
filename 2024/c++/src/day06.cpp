a6d308104296d3613b45fd4df6490bc788cd907e
Anshuman Dash <anshuman@ucsb.edu>
2024-12-08 03:38:52 +0000

    ugly slow

--- 2024/c++/src/day06.cpp
+++ 2024/c++/src/day06.cpp
@@ -7,19 +7,18 @@ bool inBounds(const pii& pos, const pii& highbound) {
 
 pii rotate_dir(const pii& dir) { return pii(-dir.y, dir.x); }
 
-bool part2(const input_t& inp, const set<pii>& blocks, const pii& high_bound,
-           set<pair<pii, pii>>& looping_states, pii pos, pii dir,
-           pii test_block) {
+set<pii> check_loop(const input_t& inp, const set<pii>& blocks, pii pos,
+                    pii dir, pii test_block = pii(-1, -1)) {
     set<pair<pii, pii>> seen;
-    dir = rotate_dir(dir);
+    set<pii> visited;
+    pii high_bound(inp[0].size(), inp.size());
 
     while (inBounds(pos, high_bound)) {
-        if (seen.count(make_pair(pos, dir)) ||
-            looping_states.count(make_pair(pos, dir))) {
-            looping_states.insert(seen.begin(), seen.end());
-            return true;
+        if (seen.count(make_pair(pos, dir))) {
+            return set<pii>();
         }
         seen.insert(make_pair(pos, dir));
+        visited.insert(pos);
         pii candidate(pos.x + dir.x, pos.y + dir.y);
         if (blocks.count(candidate) || candidate == test_block) {
             dir = rotate_dir(dir);
@@ -27,17 +26,15 @@ bool part2(const input_t& inp, const set<pii>& blocks, const pii& high_bound,
             pos = candidate;
         }
     }
-    return false;
+    return visited;
 }
 
 chrono::time_point<std::chrono::steady_clock> day06(input_t& inp) {
-    int p2(0);
+    int p1, p2(0);
 
-    set<pii> blocks, visited;
-    set<pair<pii, pii>> looping_states;
+    set<pii> blocks, p1visited;
     pii dir(0, -1);
     pii pos;
-    pii high_bound(inp[0].size(), inp.size());
 
     for (int y = 0; y < inp.size(); y++) {
         for (int x = 0; x < inp[y].size(); x++) {
@@ -49,26 +46,17 @@ chrono::time_point<std::chrono::steady_clock> day06(input_t& inp) {
             }
         }
     }
-    set<pii> p2parts;
 
-    while (inBounds(pos, high_bound)) {
-        visited.insert(pos);
-        pii candidate(pos.x + dir.x, pos.y + dir.y);
-        if (blocks.count(candidate)) {
-            dir = rotate_dir(dir);
-        } else {
-            if (part2(inp, blocks, high_bound, looping_states, pos, dir,
-                      candidate)) {
-                p2parts.insert(candidate);
-                p2++;
-            }
-            pos = candidate;
+    p1visited = check_loop(inp, blocks, pos, dir);
+    p1 = p1visited.size();
+
+    for (const auto& spot : p1visited) {
+        if (check_loop(inp, blocks, pos, dir, spot).empty()) {
+            p2++;
         }
     }
-    cout << p2parts.size() << endl;
-    // not 3324 lmao
 
     auto done = chrono::steady_clock::now();
-    cout << "[P1] " << visited.size() << "\n[P2] " << p2 << endl;
+    cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
     return done;
 }
