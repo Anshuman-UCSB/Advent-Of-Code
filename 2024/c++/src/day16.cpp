f626514b9fc6bcff16780fad29435767b3eb44b3
Anshuman Dash <anshuman@ucsb.edu>
2024-12-27 07:48:46 +0000

    this got too messy, I should rewrite and clean it

--- 2024/c++/src/day16.cpp
+++ 2024/c++/src/day16.cpp
@@ -1,94 +1,85 @@
 #include "AOC.h"
 
 typedef tuple<int, int, int, int> state;
+// no-distance state
+typedef tuple<int, int, int> ndstate;
+// with-from state
+typedef tuple<int, int, int, int, ndstate> wfstate;
+
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {-1, 0, 1, 0};
 
-ull solve_p1(input_t& grid) {
-    priority_queue<state, vector<state>, greater<state>> pq;
-    set<tuple<int, int, int>> seen;
+ull solve_p1(input_t& grid, map<ndstate, pair<int, set<ndstate>>>& revPath) {
+    priority_queue<wfstate, vector<wfstate>, greater<wfstate>> pq;
+    ndstate nd, fnd;
 
     int gX(grid[1].size() - 2), gY(1);
-    int dist, x, y, dir, tdir;
+    int tdir;
 
     // Start off with no direction penalty
-    pq.emplace(0, 1, grid.size() - 2, 1);
+    pq.emplace(0, 1, grid.size() - 2, 1, make_tuple(-1, -1, -1));
+
+    int bestDist = INT32_MAX;
 
     while (!pq.empty()) {
-        tie(dist, x, y, dir) = pq.top();
-        if (seen.insert(make_tuple(x, y, dir)).second) {
-            // cout << dist << ", " << x << ", " << y << ", " << dir << endl;
-            if (x == gX && y == gY) return dist;
+        auto [dist, x, y, dir, fnd] = pq.top();
+        nd = ndstate(x, y, dir);
 
-            tdir = (dir + 1) % 4;
-            if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
-                pq.emplace(dist + 1000, x, y, tdir);
-            }
-            // Avoiding undefined negative mod
-            tdir = (dir + 3) % 4;
-            if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
-                pq.emplace(dist + 1000, x, y, tdir);
-            }
-
-            if (grid[y + dy[dir]][x + dx[dir]] != '#') {
-                pq.emplace(dist + 1, x + dx[dir], y + dy[dir], dir);
-            }
+        if (dist > bestDist) {
+            pq.pop();
+            continue;
         }
-        pq.pop();
+
+        if (revPath.count(nd) == 0) {
+            revPath[nd].first = dist;
+            assert(revPath[nd].second.empty());
+            revPath[nd].second.insert(fnd);
+        } else {
+            // Should not later be finding better paths
+            assert(dist >= revPath[nd].first);
+            if (dist == revPath[nd].first) {
+                // Another way to get here
+                revPath[nd].second.insert(fnd);
+            }
+            pq.pop();
+            continue;
+        }
+        if (bestDist == INT32_MAX && x == gX && y == gY) {
+            bestDist = dist;
+            continue;
+        }
+
+        tdir = (dir + 1) % 4;
+        if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
+            pq.emplace(dist + 1000, x, y, tdir);
+        }
+        // Avoiding undefined negative mod
+        tdir = (dir + 3) % 4;
+        if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
+            pq.emplace(dist + 1000, x, y, tdir);
+        }
+
+        if (grid[y + dy[dir]][x + dx[dir]] != '#') {
+            pq.emplace(dist + 1, x + dx[dir], y + dy[dir], dir);
+        }
     }
 
-    return -1;
+    return bestDist;
 }
 
 pii step(const pii& p, const int dir) {
     return pii(p.x + dx[dir], p.y + dy[dir]);
 }
 
-bool DFS(input_t& inp, set<pii>& valid, map<tuple<int, int, int>, int>& seen,
-         pii pos, int dir, pii dest, int remaining) {
-    auto [x, y] = pos;
-    if (inp[y][x] == '#') return false;
-    if (remaining < 0) return false;
-    if (pos == dest) {
-        assert(remaining == 0);
-        valid.insert(pos);
-        return true;
-    }
-    if (remaining > seen[make_tuple(x, y, dir)]) {
-        seen[make_tuple(x, y, dir)];
-    } else if (remaining < seen[make_tuple(x, y, dir)]) {
-        return false;
-    } else {
-        return valid.count(make_tuple(x, y, dir));
-    }
-
-    bool good = false;
-    if (DFS(inp, valid, seen, step(pos, dir), dir, dest, remaining - 1)) {
-        valid.insert(pos);
-        good = true;
-    }
-    if (DFS(inp, valid, seen, pos, (dir + 1) % 4, dest, remaining - 1000)) {
-        valid.insert(pos);
-        good = true;
-    }
-    if (DFS(inp, valid, seen, pos, (dir + 3) % 4, dest, remaining - 1000)) {
-        valid.insert(pos);
-        good = true;
-    }
-    return good;
-}
-
 int solve_p2(input_t& grid, int target) {
     set<pii> valid;
-    map<tuple<int, int, int>, int> seen;
-    assert(DFS(grid, valid, seen, pii(1, grid.size() - 2), 1,
-               pii(grid[1].size() - 2, 1), target));
 
     return valid.size();
 }
 
 chrono::time_point<std::chrono::steady_clock> day16(input_t& inp) {
-    int p1(solve_p1(inp)), p2(solve_p2(inp, p1));
+    map<ndstate, pair<int, set<ndstate>>> revPath;
+    int p1(solve_p1(inp, revPath)), p2(solve_p2(inp, p1));
 
     auto done = chrono::steady_clock::now();
     cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
