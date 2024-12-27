6f45cd3c8e4ea0d31bc5676fe902853a310cd0c6
Anshuman Dash <anshuman@ucsb.edu>
2024-12-27 05:37:10 +0000

    worked move on lmao

--- 2024/c++/src/day15.cpp
+++ 2024/c++/src/day15.cpp
@@ -40,12 +40,116 @@ pii shift(input_t& grid, int px, int py, char d) {
     return pii(x, y);
 }
 
-chrono::time_point<std::chrono::steady_clock> day15(input_t& inp) {
-    ull p1(0), p2(0);
+bool isBlocked(const vector<vector<char>>& grid, set<pii>& moveSet, int x,
+               int y, const int dx, const int dy, bool neighborCall = false) {
+    if (grid[y][x] != '[' && grid[y][x] != ']' && grid[y][x] != '@') {
+        return grid[y][x] != '.';
+    } else {
+        moveSet.emplace(x, y);
+        return ((!neighborCall && dy) &&
+                ((grid[y][x] == '[' && isBlocked(grid, moveSet, x + 1, y, dx,
+                                                 dy, neighborCall = true)) ||
+                 (grid[y][x] == ']' && isBlocked(grid, moveSet, x - 1, y, dx,
+                                                 dy, neighborCall = true)))) ||
+               isBlocked(grid, moveSet, x + dx, y + dy, dx, dy);
+    }
+}
 
-    int i, j;
+pii shift_p2(vector<vector<char>>& grid, int px, int py, char d) {
+    int dx, dy;
+    set<pii> moveSet;
+    auto oldGrid(grid);
+
+    switch (d) {
+        case '>':
+            dx = 1;
+            dy = 0;
+            break;
+        case '<':
+            dx = -1;
+            dy = 0;
+            break;
+        case '^':
+            dx = 0;
+            dy = -1;
+            break;
+        case 'v':
+            dx = 0;
+            dy = 1;
+            break;
+    }
+
+    if (!isBlocked(grid, moveSet, px, py, dx, dy)) {
+        for (const auto& [x, y] : moveSet) {
+            grid[y][x] = '.';
+        }
+        for (const auto& [x, y] : moveSet) {
+            grid[y + dy][x + dx] = oldGrid[y][x];
+        }
+        px += dx;
+        py += dy;
+    }
+
+    return pii(px, py);
+}
+
+ull part2(const input_t& p1board, string instrs, int px, int py) {
+    ull p2(0);
     vector<vector<char>> board;
 
+    for (const auto& row : p1board) {
+        board.emplace_back();
+        for (char c : row) {
+            switch (c) {
+                case '#':
+                    board.back().push_back('#');
+                    board.back().push_back('#');
+                    break;
+                case 'O':
+                    board.back().push_back('[');
+                    board.back().push_back(']');
+                    break;
+                case '.':
+                    board.back().push_back('.');
+                    board.back().push_back('.');
+                    break;
+                default:  // @
+                    board.back().push_back('@');
+                    board.back().push_back('.');
+                    break;
+            }
+        }
+    }
+
+    px *= 2;
+
+    for (auto& d : instrs) {
+        // cout << d << endl;
+        tie(px, py) = shift_p2(board, px, py, d);
+        // for (auto& r : board) {
+        //     for (auto c : r) {
+        //         cout << c;
+        //     }
+        //     cout << endl;
+        // }
+        // break;
+    }
+
+    for (int y = 0; y < board.size(); y++) {
+        for (int x = 0; x < board[0].size(); x++) {
+            if (board[y][x] == '[') {
+                p2 += y * 100 + x;
+            }
+        }
+    }
+
+    return p2;
+}
+
+chrono::time_point<std::chrono::steady_clock> day15(input_t& inp) {
+    ull p1(0);
+
+    int i, j;
     // Cursed syntax lol
     do i++;
     while (!inp[i].empty());
@@ -75,6 +179,9 @@ chrono::time_point<std::chrono::steady_clock> day15(input_t& inp) {
             }
         }
     }
+
+    ull p2 = part2(inp, instrs, px, py);
+
     for (auto& c : instrs) {
         tie(px, py) = shift(inp, px, py, c);
         // break;
