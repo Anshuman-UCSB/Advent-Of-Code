21f237cb1d7cf587021afb04ce45e680c2fe422f
Anshuman Dash <anshuman@ucsb.edu>
2024-12-30 10:41:01 +0000

    LETS GOOO FINALLY

--- 2024/c++/src/day16.cpp
+++ 2024/c++/src/day16.cpp
@@ -1,87 +1,97 @@
 #include "AOC.h"
 
-typedef tuple<int, int, int, int> state;
-// no-distance state
-typedef tuple<int, int, int> ndstate;
-// with-from state
-typedef tuple<int, int, int, int, ndstate> wfstate;
-
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {-1, 0, 1, 0};
-
-ull solve_p1(input_t& grid, map<ndstate, pair<int, set<ndstate>>>& revPath) {
-    priority_queue<wfstate, vector<wfstate>, greater<wfstate>> pq;
-    ndstate nd, fnd;
-
-    int gX(grid[1].size() - 2), gY(1);
-    int tdir;
-
-    // Start off with no direction penalty
-    pq.emplace(0, 1, grid.size() - 2, 1, make_tuple(-1, -1, -1));
-
-    int bestDist = INT32_MAX;
-
-    while (!pq.empty()) {
-        auto [dist, x, y, dir, fnd] = pq.top();
-        nd = ndstate(x, y, dir);
-
-        if (dist > bestDist) {
-            pq.pop();
-            continue;
-        }
-
-        if (revPath.count(nd) == 0) {
-            revPath[nd].first = dist;
-            assert(revPath[nd].second.empty());
-            revPath[nd].second.insert(fnd);
-        } else {
-            // Should not later be finding better paths
-            assert(dist >= revPath[nd].first);
-            if (dist == revPath[nd].first) {
-                // Another way to get here
-                revPath[nd].second.insert(fnd);
-            }
-            pq.pop();
-            continue;
-        }
-        if (bestDist == INT32_MAX && x == gX && y == gY) {
+// dist, point, direction, from, from direction
+typedef tuple<int, pii, int, pii, int> state;
+class FromCache {
+   public:
+    int bestDist;
+    set<pair<pii, int>> from;
+    FromCache() { bestDist = INT32_MAX; }
+    bool reg(pii p, int fromdir, int dist) {
+        if (dist < bestDist) {
             bestDist = dist;
-            continue;
+            from.clear();
+            from.emplace(p, fromdir);
+        } else if (dist == bestDist) {
+            from.emplace(p, fromdir);
+        } else {
+            return false;
         }
-
-        tdir = (dir + 1) % 4;
-        if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
-            pq.emplace(dist + 1000, x, y, tdir);
-        }
-        // Avoiding undefined negative mod
-        tdir = (dir + 3) % 4;
-        if (grid[y + dy[tdir]][x + dx[tdir]] != '#') {
-            pq.emplace(dist + 1000, x, y, tdir);
-        }
-
-        if (grid[y + dy[dir]][x + dx[dir]] != '#') {
-            pq.emplace(dist + 1, x + dx[dir], y + dy[dir], dir);
-        }
+        return true;
     }
+    bool wasSeen() { return bestDist < INT32_MAX; }
+};
 
-    return bestDist;
-}
-
-pii step(const pii& p, const int dir) {
+char at(const input_t& inp, const pii& p) { return inp[p.y][p.x]; }
+pii step(const pii& p, int dir) {
+    const int dx[] = {0, 1, 0, -1};
+    const int dy[] = {-1, 0, 1, 0};
     return pii(p.x + dx[dir], p.y + dy[dir]);
 }
 
-int solve_p2(input_t& grid, int target) {
-    set<pii> valid;
-
-    return valid.size();
-}
-
 chrono::time_point<std::chrono::steady_clock> day16(input_t& inp) {
-    map<ndstate, pair<int, set<ndstate>>> revPath;
-    int p1(solve_p1(inp, revPath)), p2(solve_p2(inp, p1));
+    int p2(0), best(INT32_MAX);
+    pii start(1, inp.size() - 2), end(inp[1].size() - 2, 1);
+    priority_queue<state, vector<state>, greater<state>> pq;
+    pq.emplace(0, start, 1, pii(-1, -1), -1);
+
+    map<pair<pii, int>, FromCache> fromCache;
+
+    while (!pq.empty()) {
+        auto [dist, p, dir, from, fromdir] = pq.top();
+        auto pos_state = make_pair(p, dir);
+
+        fromCache[pos_state].reg(from, fromdir, dist);
+        // cout << fromCache[pos_state].bestDist << endl;
+        if (dist <= best && dist <= fromCache[pos_state].bestDist) {
+            // cout << dist << "\t" << p << dir << "\tfrom: " << from << fromdir
+            //      << '\t' << best << endl;
+            if (p == end) {
+                if (best == INT32_MAX) {
+                    best = dist;
+                }
+            } else {
+                if (at(inp, step(p, dir)) != '#') {
+                    pq.emplace(dist + 1, step(p, dir), dir, p, dir);
+                }
+                if (at(inp, step(p, (dir + 1) % 4)) != '#') {
+                    pq.emplace(dist + 1000, p, (dir + 1) % 4, p, dir);
+                }
+                if (at(inp, step(p, (dir + 3) % 4)) != '#') {
+                    pq.emplace(dist + 1000, p, (dir + 3) % 4, p, dir);
+                }
+            }
+        }
+        pq.pop();
+    }
+
+    queue<pair<pii, int>> q;
+    q.emplace(end, 0);
+    q.emplace(end, 1);
+    set<pii> p2set;
+    while (!q.empty()) {
+        auto tmp = q.front();
+        // cout << tmp << endl;
+        q.pop();
+        if (tmp.first.x == -1) continue;
+        p2set.insert(tmp.first);
+        for (auto& v : fromCache[tmp].from) {
+            q.push(v);
+        }
+    }
+    p2 = p2set.size();
+
+    // for (auto& p : p2set) {
+    //     inp[p.y][p.x] = 'O';
+    // }
+    // for (auto& r : inp) {
+    //     cout << r << endl;
+    // }
+
+    // cout << fromCache[make_pair(pii(5, 7), 0)].from
+    //      << fromCache[make_pair(pii(5, 7), 0)].bestDist << endl;
 
     auto done = chrono::steady_clock::now();
-    cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
+    cout << "[P1] " << best << "\n[P2] " << p2 << endl;
     return done;
 }
