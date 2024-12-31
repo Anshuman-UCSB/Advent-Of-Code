9c2db1a21d64ffccf93d07e99bac457fc7ac2fd4
Anshuman Dash <anshuman@ucsb.edu>
2024-12-31 23:23:15 +0000

    p1 done:

--- 2024/c++/src/day20.cpp
+++ 2024/c++/src/day20.cpp
@@ -1,11 +1,84 @@
 #include "AOC.h"
 
-chrono::time_point<std::chrono::steady_clock> day20(input_t& inp){
-	int p1(0), p2(0);
+// dist, pos
+typedef tuple<int, pii> rev_state;
+vector<pii> dirs = {pii(-1, 0), pii(0, -1), pii(1, 0), pii(0, 1)};
 
-	
-	
-	auto done = chrono::steady_clock::now();
-	cout<<"[P1] "<<p1<<"\n[P2] "<<p2<<endl;
-	return done;
+chrono::time_point<std::chrono::steady_clock> day20(input_t& inp) {
+    int p1(0), p2(0);
+
+    pii start, end;
+    for (int y = 0; y < inp.size(); y++) {
+        for (int x = 0; x < inp[0].size(); x++) {
+            switch (inp[y][x]) {
+                case 'S':
+                    start = pii(x, y);
+                    break;
+                case 'E':
+                    end = pii(x, y);
+                    break;
+            }
+        }
+    }
+
+    queue<rev_state> q;
+    q.emplace(0, end);
+
+    map<pii, int> rev_dist;
+    while (!q.empty()) {
+        auto [d, p] = q.front();
+        q.pop();
+        if (rev_dist.count(p)) continue;
+        rev_dist[p] = d;
+        for (auto& dir : dirs) {
+            pii t(p.x + dir.x, p.y + dir.y);
+            if (inp[t.y][t.x] != '#') {
+                q.emplace(d + 1, t);
+            }
+        }
+    }
+    // cout << rev_dist[start] << endl;
+
+    // need 2 clear spaces to jump regardless
+    for (int y = 1; y < inp.size() - 1; y++) {
+        for (int x = 1; x < inp.size() - 1; x++) {
+            if (inp[y][x] != '#') {
+                for (const auto& dir : dirs) {
+                    if (inp[y + dir.y][x + dir.x] == '#' && y + dir.y > 0 &&
+                        y + dir.y < inp.size() - 1 && x + dir.x > 0 &&
+                        x + dir.x < inp.size() - 1 &&
+                        inp[y + dir.y * 2][x + dir.x * 2] != '#') {
+                        // skip found
+                        int timeSave =
+                            rev_dist[pii(x, y)] -
+                            rev_dist[pii(x + dir.x * 2, y + dir.y * 2)] - 2;
+                        // -2 because you need to cheat through wall
+                        // if (timeSave >= 0) {
+                        //     cout << "Shortcut from " << pii(x, y) << " to "
+                        //          << pii(x + dir.x * 2, y + dir.y * 2)
+                        //          << " saves " << timeSave << endl;
+                        // }
+                        if (timeSave >= 100) {
+                            p1++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // for (int y = 0; y < inp.size(); y++) {
+    //     for (int x = 0; x < inp[0].size(); x++) {
+    //         if (inp[y][x] == '#') {
+    //             cout << '#';
+    //         } else {
+    //             cout << rev_dist[pii(x, y)] % 10;
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    auto done = chrono::steady_clock::now();
+    cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
+    return done;
 }
