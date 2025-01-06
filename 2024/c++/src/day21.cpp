32a8ae78f8d58621f357646c6b705ea661ee215a
Anshuman Dash <anshuman@ucsb.edu>
2025-01-06 10:41:42 +0000

    can solve numpad

--- 2024/c++/src/day21.cpp
+++ 2024/c++/src/day21.cpp
@@ -21,56 +21,65 @@ map<char, pii> dirs = {{'>', pii(1, 0)},
 pii stepDir(pii pos, pii dir) { return pii(pos.x + dir.x, pos.y + dir.y); }
 
 char getChar(const string mapping[], const pii pos) {
-    // cout << npos << endl;
     return mapping[pos.y][pos.x];
 }
 
-pair<pii, char> sim(const string sim_mapping[], pii pos, char action) {
-    pii newpos = stepDir(pos, dirs[action]);
-    char c = getChar(sim_mapping, newpos);
-    if (c == '#') return make_pair(pii(), '#');
-    if (action == 'A') return make_pair(pos, c);
-    return make_pair(newpos, '\0');
-}
-
-string solve_p1(string target) {
-    queue<state> q;
-    q.emplace("", pii(3, 1), pii(3, 1), pii(3, 4), "");
-    while (!q.empty()) {
-        auto [inp, kpos2, kpos1, numpos, out] = q.front();
-        cout << inp << ',' << kpos2 << ',' << kpos1 << ',' << numpos << ','
-             << out << endl;
-        q.pop();
-
-        if (!target.starts_with(out)) continue;
-        for (auto& [k, v] : dirs) {
-            string new_out = out;
-            pii nkpos2(kpos2), nkpos1(kpos1), nnumpos(numpos);
-            char na2, na1, nout;
-            tie(nkpos2, na2) = sim(KEYPAD, kpos2, k);
-            if (na2 == '#') continue;  // if leads to invalid
-            if (na2) {
-                tie(nkpos1, na1) = sim(KEYPAD, kpos1, na2);
-                if (na1 == '#') continue;
-                if (na1) {
-                    auto [nnumpos, nout] = sim(NUMPAD, numpos, na1);
-                    if (nout == '#') continue;  // if leads to invalid
-                    if (nout) {
-                        new_out.push_back(nout);
-                    }
-                }
+// returns perfect match, partial match, and output string
+tuple<bool, bool, string> checkMatch(const string MAP[], string inp,
+                                     string out) {
+    string gen_string = "";
+    pii pos = (MAP == NUMPAD) ? pii(3, 4) : pii(3, 1);
+    for (auto i : inp) {
+        // cout << i << ": ";
+        if (i == 'A') {
+            char c = getChar(MAP, pos);
+            gen_string.push_back(c);
+            // cout << gen_string << endl;
+            if (!out.starts_with(gen_string)) {
+                return make_tuple(false, false, gen_string);
             }
-            q.emplace(inp + k, nkpos2, nkpos1, nnumpos, new_out);
+        } else {
+            pos = stepDir(pos, dirs[i]);
+            // cout << pos << endl;
+            if (getChar(MAP, pos) == '#')
+                return make_tuple(false, false, gen_string);
         }
     }
-    return "fail";
+
+    return make_tuple(gen_string == out, true, gen_string);
+}
+
+string solve_p1(string target) { return "fail"; }
+
+string solve_numpad(string target) {
+    queue<string> q;
+    size_t farthest = 0;
+    q.emplace("");
+    while (!q.empty()) {
+        string inp = q.front();
+        q.pop();
+        auto [full, partial, gen] = checkMatch(NUMPAD, inp, target);
+        if (!partial) continue;
+        farthest = max(gen.size(), farthest);
+        if (gen.size() < farthest) continue;
+        // cout << inp << ": " << make_tuple(full, partial, gen) << endl;
+        if (full) return inp;
+        if (partial) {
+            for (auto& c : {'A', '<', '^', 'v', '>'}) {
+                q.emplace(inp + c);
+            }
+        }
+    }
+    return "FAIL";
 }
 
 chrono::time_point<std::chrono::steady_clock> day21(input_t& inp) {
     int p1(0), p2(0);
-    for (auto& l : inp) {
-        cout << l << ": " << solve_p1(l) << endl;
-    }
+    // for (auto& l : inp) {
+    //     cout << l << ": " << solve_p1(l) << endl;
+    // }
+    cout << checkMatch(NUMPAD, "<A^A>^^AvvA", "029A") << endl;
+    cout << solve_numpad("029A") << endl;
 
     auto done = chrono::steady_clock::now();
     cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
