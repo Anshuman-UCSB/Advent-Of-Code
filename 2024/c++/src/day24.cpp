1bb746d6f6c67308886108c94b57381cda7593ed
Anshuman Dash <anshuman@ucsb.edu>
2025-01-08 04:27:19 +0000

    WE BALL

--- 2024/c++/src/day24.cpp
+++ 2024/c++/src/day24.cpp
@@ -1,16 +1,70 @@
 #include "AOC.h"
 
+class Component;
 class Wire {
+   public:
     string name;
     bool val;
     bool defined;
-    set<Wire> connections;
+    vector<Component> connections;
 
-   public:
     Wire() {}
-    Wire(string name) : name(name), defined(false) {}
-    friend ostream& operator<<(ostream& os, const Wire& w);
+    Wire(string name);
+
+    void update(bool value);
 };
+
+class Component {
+   public:
+    string type;
+    Wire *a, *b, *out;
+    bool completed;
+
+    Component(string line);
+    void update();
+};
+
+map<string, Wire*> wires;
+
+Wire::Wire(string name) : name(name), defined(false) {
+    // cout << "Made wire with name " << name << endl;
+}
+void Wire::update(bool value) {
+    if (defined) return;
+    defined = true;
+    val = value;
+}
+
+Component::Component(string line) {
+    stringstream ss(line);
+    string na, nb, nout;
+    ss >> na >> type >> nb >> nout >> nout;
+
+    if (wires.find(na) == wires.end()) wires[na] = new Wire(na);
+    if (wires.find(nb) == wires.end()) wires[nb] = new Wire(nb);
+    if (wires.find(nout) == wires.end()) wires[nout] = new Wire(nout);
+
+    a = wires[na];
+    b = wires[nb];
+    out = wires[nout];
+    completed = out->defined;
+
+    a->connections.push_back(*this);
+    b->connections.push_back(*this);
+}
+
+void Component::update() {
+    if (completed) return;
+    if (a->defined && b->defined) {
+        if (type == "AND") {
+            out->update(a->val & b->val);
+        } else if (type == "OR") {
+            out->update(a->val | b->val);
+        } else if (type == "XOR") {
+            out->update(a->val ^ b->val);
+        }
+    }
+}
 
 ostream& operator<<(ostream& os, const Wire& w) {
     os << "(" << w.name << "-";
@@ -19,27 +73,36 @@ ostream& operator<<(ostream& os, const Wire& w) {
     } else {
         os << w.val ? "1" : "0";
     }
-    os << " <";
+    // os << " <";
 
-    string delim;
-    for (auto& e : w.connections) {
-        os << delim << e.name;
-        delim = ", ";
-    }
+    // string delim;
+    // for (auto& e : w.connections) {
+    //     os << delim << e.a->name << e.type << e.b->name << " -> "
+    //        << e.out->name;
+    //     os << e.type << endl;
+    //     delim = ", ";
+    // }
 
-    os << ">";
+    // os << ">";
+    os << ")";
     return os;
 }
-
-map<string, Wire> wires;
-void registerWire(string name) { wires[name] = Wire(name); }
 
 chrono::time_point<std::chrono::steady_clock> day24(input_t& inp) {
     int p1(0), p2(0);
 
-    registerWire("x00");
-    registerWire("y00");
-    cout << wires << endl;
+    // Wire("x00");
+    // Wire("y00");
+    Component c("x00 AND y00 -> z00");
+    for (auto& [k, v] : wires) {
+        cout << k << "\t" << *v << endl;
+    }
+
+    // cout << c.completed << endl;
+    // wires["x00"].update(1);
+    // cout << c.completed << endl;
+    // wires["y00"].update(1);
+    // cout << c.completed << endl;
 
     auto done = chrono::steady_clock::now();
     cout << "[P1] " << p1 << "\n[P2] " << p2 << endl;
